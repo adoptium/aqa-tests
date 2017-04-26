95d6f9c0e6547c34e507227b78bf6a9eb5242d7a
Sophia Guo <sophiag@ca.ibm.com>
2017-04-26 14:06:19 -0400

    update get.sh support wget third party jars in local
    Signed-off-by: Sophia Guo <sophiag@ca.ibm.com>

--- get.sh
+++ get.sh
@@ -12,19 +12,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-echo 'Get binary openjdk...'
-cd $1
-mkdir openjdkbinary
-cd openjdkbinary
-download_url=`curl https://api.github.com/repos/AdoptOpenJDK/openjdk-releases/releases/latest | jq -r '.assets[] | select(.browser_download_url | contains("x64_Linux_jdk")) | {url: .browser_download_url} | select (.url | contains("tar.gz")) | .[]'`
-wget "${download_url}"
-#wget https://github.com/AdoptOpenJDK/openjdk-releases/releases/download/jdk8u152-b01/OpenJDK8_x64_Linux_jdk8u152-b01.tar.gz
-jar_file_url=$download_url
-jar_file_name=${jar_file_url##*/}
-tar -zxvf $jar_file_name
-#tar -zxvf OpenJDK8_x64_Linux_jdk8u152-b01.tar.gz
+openjdktest=$(pwd)
+if [ "$#" -eq 2 ]
+then
+	openjdktest=$2
+	echo 'Get binary openjdk...'
+	cd $1
+	mkdir openjdkbinary
+	cd openjdkbinary
+	download_url=`curl https://api.github.com/repos/AdoptOpenJDK/openjdk-releases/releases/latest | jq -r '.assets[] | select(.browser_download_url | contains("x64_Linux_jdk")) | {url: .browser_download_url} | select (.url | contains("tar.gz")) | .[]'`
+	wget "${download_url}"
+	#wget https://github.com/AdoptOpenJDK/openjdk-releases/releases/download/jdk8u152-b01/OpenJDK8_x64_Linux_jdk8u152-b01.tar.gz
+	jar_file_url=$download_url
+	jar_file_name=${jar_file_url##*/}
+	tar -zxvf $jar_file_name
+	#tar -zxvf OpenJDK8_x64_Linux_jdk8u152-b01.tar.gz
+elif [ "$#" -eq 1 ]; then
+	openjdktest=$1
+fi
 
-cd $2/TestConfig
+cd $openjdktest/TestConfig
 mkdir lib
 cd lib
 echo 'Get third party libs...'
@@ -38,5 +45,6 @@ wget http://central.maven.org/maven2/org/testng/testng/6.10/testng-6.10.jar
 wget http://central.maven.org/maven2/com/beust/jcommander/1.48/jcommander-1.48.jar
 mv testng-6.10.jar testng.jar
 mv jcommander-1.48.jar jcommander.jar
+
 
 
