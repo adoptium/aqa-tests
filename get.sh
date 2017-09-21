d5ca948a4ba7c1887e604e6e6156c738a8f0e854
sophia-guo <sophiag@ca.ibm.com>
2017-09-21 15:45:01 -0400

    Add wget Header option to avoid ERROR 403. (#101)
    Also add status check, turn off the verbose output
    
    Signed-off-by: Sophia Guo <sophiag@ca.ibm.com>

--- get.sh
+++ get.sh
@@ -30,9 +30,13 @@ else
 	mkdir openjdkbinary
 	cd openjdkbinary
 	download_url="https://api.adoptopenjdk.net/$jvm_version/releases/$platform/latest/binary"
-	wget --no-check-certificate ${download_url}
+	wget -q --no-check-certificate --header 'Cookie: allow-download=1' ${download_url}
+	if [ $? -ne 0 ]; then
+		echo "Failed to retrieve the jdk binary, exiting"
+		exit
+	fi
 	jar_file_name=`ls`
-	tar -zxvf $jar_file_name
+	tar -zxf $jar_file_name
 	sdkDir=`ls -d */`
 	sdkDirName=${sdkDir%?}
 	mv $sdkDirName j2sdk-image
@@ -42,20 +46,20 @@ cd $openjdktest/TestConfig
 mkdir lib
 cd lib
 echo 'Get third party libs...'
-wget http://download.forge.ow2.org/asm/asm-5.0.1.jar
-wget https://downloads.sourceforge.net/project/junit/junit/4.10/junit-4.10.jar
+wget -q http://download.forge.ow2.org/asm/asm-5.0.1.jar
+wget -q https://downloads.sourceforge.net/project/junit/junit/4.10/junit-4.10.jar
 mv asm-5.0.1.jar asm-all-5.0.1.jar
 
 mkdir TestNG
 cd TestNG
-wget http://central.maven.org/maven2/org/testng/testng/6.10/testng-6.10.jar
-wget http://central.maven.org/maven2/com/beust/jcommander/1.48/jcommander-1.48.jar
+wget -q http://central.maven.org/maven2/org/testng/testng/6.10/testng-6.10.jar
+wget -q http://central.maven.org/maven2/com/beust/jcommander/1.48/jcommander-1.48.jar
 mv testng-6.10.jar testng.jar
 mv jcommander-1.48.jar jcommander.jar
 
 cd $openjdktest/TestConfig/lib
 echo 'get jtreg...'
-wget --no-check-certificate https://ci.adoptopenjdk.net/job/jtreg/lastSuccessfulBuild/artifact/jtreg-4.2.0-tip.tar.gz
+wget -q --no-check-certificate https://ci.adoptopenjdk.net/job/jtreg/lastSuccessfulBuild/artifact/jtreg-4.2.0-tip.tar.gz
 if [ $? -ne 0 ]; then
 	echo "Failed to retrieve the jtreg binary, exiting"
 exit
@@ -70,7 +74,7 @@ if [[ $jvm_version =~ "openjdk9" ]]; then
 	openjdkGit="openjdk-jdk9"
 fi
 
-git clone -b dev https://github.com/AdoptOpenJDK/$openjdkGit.git
+git clone -b dev -q https://github.com/AdoptOpenJDK/$openjdkGit.git
 
 openjdkDir=`ls -d */`
 openjdkDirName=${openjdkDir%?}
