34f260820638ca31cde0f5f6ed21d28449d78b89
Tianyu Zuo <tianyu@ca.ibm.com>
2017-09-27 17:33:09 -0400

    integrate dockerized scala test with testKitGen
    * Add thirdparty_containers folder for all dockerized test
    * Add scala under thirdparty_containers project
    * build.xml is used to build scala container test docker image
    * playlist.xml is used to execute the scala container test image
    * scala container test now run "jvm, pos and net" test suite
    * more test suites can be added if needed
    * the scala container test can now be built and triggered by testkitGen
    * To build and run scala test, you need to export environment variable:
      - export BUILD_LIST="containerTest/scala-test"
      - export SCALA_ROOT=<full path to Scala source root>
    
    
    Signed-off-by: Tianyu Zuo <tianyu@ca.ibm.com>

---
+++ thirdparty_containers/scala/dockerfile/scala-test.sh
@@ -0,0 +1,57 @@
+#/bin/bash
+#
+# (C) Copyright IBM Corporation 2017.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+if [ -d /java/jre/bin ];then
+	echo "Using mounted Java8"
+	export JAVA_BIN=/java/jre/bin
+	export JAVA_HOME=/java
+	export PATH=$JAVA_BIN:$PATH
+	java -version
+elif [ -d /java/bin ]; then
+	echo "Using mounted Java9"
+	export JAVA_BIN=/java/bin
+	export JAVA_HOME=/java
+	export PATH=$JAVA_BIN:$PATH
+	java -version
+else
+	echo "Using docker image default Java"
+	java_path=$(type -p java)
+	suffix="/java"
+	java_root=${java_path%$suffix}
+	export JAVA_BIN="$java_root"
+	echo "JAVA_BIN is: $JAVA_BIN"
+	$JAVA_BIN/java -version
+fi
+
+TEST_SUITE=$1
+export JAVA_TOOL_OPTIONS="-Dfile.encoding=UTF8"
+#begin scala test
+echo "PATH is : $PATH"
+echo "JAVA_HOME is : $JAVA_HOME"
+echo "type -p java is :"
+type -p java
+echo "java -version is: \n"
+java -version
+cd /scala
+ls .
+pwd
+echo "Begin to execute Scala test with cmd: sbt \"partest $TEST_SUITE\"" && \
+echo "Try to echo Scala version by using sbt \"scala -version\"" && \
+sbt "scala -version"
+
+echo "Begin to execute Scala test with cmd: sbt \"partest $TEST_SUITE\"" && \
+sbt "partest $TEST_SUITE"
