8dde10745c110e529b84c1025ab8732619b6a2d1
Mesbah Alam <Mesbah_Alam@ca.ibm.com>
2018-11-26 11:32:23 -0500

    Add systemtest mk file to include changes to enable testing on cygwin
    Signed-off-by: Mesbah Alam <Mesbah_Alam@ca.ibm.com>

---
+++ systemtest/systemtest.mk
@@ -0,0 +1,40 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Environment variable OSTYPE is set to cygwin if running under cygwin.
+# Set our own macro to indicate we're running under cygwin.
+ifndef CYGWIN
+  OSTYPE?=$(shell echo $$OSTYPE)
+  CYGWIN:=0
+  ifeq ($(OSTYPE),cygwin)
+    CYGWIN:=1
+  endif
+  ifeq ($(TERM),cygwin)
+    CYGWIN:=1
+  endif
+endif
+$(warning CYGWIN is $(CYGWIN))
+
+ifeq ($(CYGWIN),1)
+   # If we are running under cygwin, the tests need to run with a Windows perl port (e.g. Strawberry perl) rather
+   # than the cygwin perl port. This assumes that version will be in a directory ending /perl/bin directory
+   # and the cygwin version will not. Once found, that version of perl is added ahead of cygwin perl in the PATH.
+   $(warning Running under cygwin, looking for Windows perl on path)
+   PERL:=$(shell which -a perl.exe | grep /perl/bin | sort | uniq)
+   ifeq (,$(PERL))
+     $(error Unable to find Windows perl e.g. Strawberry perl in a /perl/bin subdirectory on PATH.  Install perl or add to PATH and retry)
+   else
+     $(warning Found perl in $(PERL))
+   endif
+   PERL:=$(dir $(PERL))
+   export PATH:=$(PERL):$(PATH)
+endif
