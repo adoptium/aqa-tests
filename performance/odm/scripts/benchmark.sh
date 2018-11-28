e67009eef3942bafbb81194f0617f9b380e71bf8
piyush286 <piyush286@gmail.com>
2018-11-28 16:40:25 -0500

    Deploy ODM Benchmark
    • Enabled ODM benchmark since benchmark package has been staged (https://github.com/AdoptOpenJDK/openjdk-tests/issues/457) and numactl has been installed (https://github.com/AdoptOpenJDK/openjdk-infrastructure/issues/394) on the perf machines
    • Fixed various environment variables
    • Updated the playlist to copy the benchmark config file in the correct directory before running it
    • Cleaned up some redundant code
    • Other fixes
    
    Signed-off-by: Piyush <piyush@ca.ibm.com>

--- performance/odm/scripts/benchmark.sh
+++ performance/odm/scripts/benchmark.sh
@@ -14,40 +14,23 @@
 
 echo "***** Running Benchmark Script *****"
 
-export PRODUCT=pxa6480sr6
-export BM_DIR_NAME=ilog_wodm881
-
-#Just for testing purposes: to be removed later
 currentDir="$(pwd)"
 echo "$currentDir"
 echo "Current Dir: $PWD"
-ls -lart
-echo "$PERFMAN_PASSWORD"
-
-#This is where all the benchmark packages are downloaded. All downloaded SDKs exist under ./sdks
-RUN_DIR=$1
-export RUN_DIR=$RUN_DIR
 
 #TODO: Remove these once the use of STAF has been eliminated from all the benchmark scripts
 export PATH=/usr/local/staf/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/staf/lib:$LD_LIBRARY_PATH
 
-benchmark_dir="$RUN_DIR/${BM_DIR_NAME}"
+benchmark_dir="${currentDir}"
 echo "benchmark_dir: $benchmark_dir"
 
-cd $benchmark_dir
-echo "Current Dir: $(pwd)"
-
-#Temporarily setting the SDK to this since Perfxposh doesn't have Espresso access so we have use the one that's already
-#there on the machine.
-LAST_BUILD=`cat "$WORKSPACE/JDK_LAST_BUILD.txt"`
-#export JDK_DIR="$WORKSPACE/../../sdks"
-#export JDK="${PRODUCT}-$LAST_BUILD"
-JDK_DIR=$JAVA_BIN/..
+JDK_DIR=$JAVA_BIN/../..
 export JDK_DIR=${JDK_DIR}
 
 ######### Generated Script #########
 
+#TODO: Need to do some cleanup and restructure some files for adding other configs
 export CLASS="com.ibm.rules.bench.segmentation.RuleEngineRunner"
 export CLSPATH="lib/asm-3.1.jar:lib/asm-analysis-3.1.jar:lib/asm-commons-3.1.jar:lib/asm-tree-3.1.jar:lib/asm-util-3.1.jar:lib/dom4j-1.6.1.jar:lib/j2ee_connector-1_5-fr.jar:lib/jrules-engine.jar:lib/jrules-res-execution.jar:lib/log4j-1.2.8.jar:lib/openxml4j-beta.jar:lib/sam.jar:lib/sizing-xom.jar:bin:bin/ra.xml"
 export RULEUSAGE="false"
@@ -62,10 +45,11 @@ export MAXHEAPSIZE="-Xmx1024m"
 export TENUREDSIZE=""
 export NURSERYSIZE=""
 export MULTITHREAD="4"
+
+#TODO: Need to soft-code these configs
 export CPUAFFINITY="numactl --physcpubind=0,1,36,37 --membind=0"
 
 echo "********** START OF NEW TESTCI BENCHMARK JOB **********"
 echo "Benchmark Name: ILOG_WODM Benchmark Variant: 881-4way-Seg5FastpathRVEJB"
-echo "Benchmark Product: $JDK"
 
 bash ./run_ilog_with_gcmv.sh
