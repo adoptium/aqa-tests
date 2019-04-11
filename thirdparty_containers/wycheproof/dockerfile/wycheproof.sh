69f85892ff48476b71c9bb0e91cc421821c0fbcb
Mesbah_Alam@ca.ibm.com <Mesbah_Alam@ca.ibm.com>
2019-04-11 15:44:17 -0400

    Add Wycheproof OpenJDKAllTests to the external test pool
    Signed-off-by: Mesbah_Alam@ca.ibm.com <Mesbah_Alam@ca.ibm.com>

---
+++ thirdparty_containers/wycheproof/dockerfile/wycheproof.sh
@@ -0,0 +1,49 @@
+#/bin/bash
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+if [ -d /java/jre/bin ];then
+	echo "Using mounted Java8"
+	export JAVA_BIN=/java/jre/bin
+	export JAVA_HOME=/java
+	export PATH=$JAVA_BIN:$PATH
+elif [ -d /java/bin ]; then
+	echo "Using mounted Java"
+	export JAVA_BIN=/java/bin
+	export JAVA_HOME=/java
+	export PATH=$JAVA_BIN:$PATH
+else
+	echo "Using docker image default Java"
+	java_path=$(type -p java)
+	suffix="/java"
+	java_root=${java_path%$suffix}
+	export JAVA_BIN="$java_root"
+	echo "JAVA_BIN is: $JAVA_BIN"
+	export JAVA_HOME="${java_root%/bin}"
+fi
+
+java -version
+
+TEST_OPTIONS=$1
+
+#Run tests 
+
+cd ${WYCHEPROOF_HOME}/wycheproof
+
+#Run OpenJDKAllTests 
+bazel test OpenJDKTest --genrule_strategy=standalone --spawn_strategy=standalone --verbose_failures
+
+#Run BouncyCastleAllTests tests 
+bazel test BouncyCastleTest --genrule_strategy=standalone --spawn_strategy=standalone --verbose_failures
+
+find /root/.cache -type d -name 'testlogs' -exec cp -r "{}" /testResults \;
