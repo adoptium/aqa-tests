730566459ec37462563afa3d4841c91bf376e353
sophia-guo <sophiag@ca.ibm.com>
2019-06-27 13:39:59 -0400

    Retain files that match a specific filename to help debug (#1196)
    Signed-off-by: Sophia Guo <sophiag@ca.ibm.com>

--- openjdk/openjdk.mk
+++ openjdk/openjdk.mk
@@ -34,7 +34,7 @@ JTREG_BASIC_OPTIONS += $(JTREG_ASSERT_OPTION)
 # Report details on all failed or error tests, times, and suppress output for tests that passed
 JTREG_BASIC_OPTIONS += -v:fail,error,time,nopass
 # Retain all files for failing tests
-JTREG_BASIC_OPTIONS += -retain:fail,error
+JTREG_BASIC_OPTIONS += -retain:fail,error,*.dmp,javacore.*,heapdump.*,*.trc
 # Ignore tests are not run and completely silent about it
 JTREG_IGNORE_OPTION = -ignore:quiet
 JTREG_BASIC_OPTIONS += $(JTREG_IGNORE_OPTION)
