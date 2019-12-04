e2892182fb4c28f58ca796c5df982e3899e075c7
Kentaro Kuramochi <kkes335@jp.ibm.com>
2019-12-04 16:59:47 +0900

    Remove disabled tags from playlist.xml

--- functional/MBCS_Tests/tools/set_variable.sh
+++ functional/MBCS_Tests/tools/set_variable.sh
@@ -13,26 +13,8 @@
 # limitations under the License.
 ################################################################################
 
-BASE=`dirname $0`
-export BASE
-CP="-cp ${BASE}/unicode.jar"
-. ${BASE}/set_variable.sh
-
-${JAVA_BIN}/java ${CP} UnicodeChecker        2>err1.txt
-cat err1.txt
-${JAVA_BIN}/java ${CP} UnicodeBlockChecker   2>err2.txt
-cat err2.txt
-${JAVA_BIN}/java ${CP} UnicodeScriptChecker  2>err3.txt
-cat err3.txt
-${JAVA_BIN}/java ${CP} UnicodeScriptChecker3 2>err4.txt
-cat err4.txt
-${JAVA_BIN}/java ${CP} NormalizerTest        2>err5.txt
-cat err5.txt
-
-if [ -s err1.txt ] || [ -s err2.txt ] || [ -s err3.txt ] || [ -s err4.txt ] || [ -s err5.txt ]; then
-  echo Test Failed
-  exit 1
+if [ -x "${TEST_JDK_HOME}/jre/bin/java" ] ; then
+    export JAVA_BIN=${TEST_JDK_HOME}/jre/bin
 else
-  echo Test Passed
-  exit 0
+    export JAVA_BIN=${TEST_JDK_HOME}/bin
 fi
