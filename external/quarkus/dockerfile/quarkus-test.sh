7f097a580ebc12659cf812f603145521c6c67f36
sophia-guo <sophiag@ca.ibm.com>
2020-01-09 13:32:28 -0500

    Add quakus tests (#1533)
    * Initial quarkus test project
    
    Signed-off-by: smlambert <slambert@gmail.com>
    
    * Set MAVEN_OPTS
    
    Signed-off-by: smlambert <slambert@gmail.com>
    
    * MaxPermSize not used in 8+
    
    Signed-off-by: smlambert <slambert@gmail.com>
    
    * Separate java & native test
    
    Signed-off-by: smlambert <slambert@gmail.com>
    
    * update where mode gets passed/set
    
    Signed-off-by: smlambert <slambert@gmail.com>
    
    * remove mode for now
    
    Signed-off-by: smlambert <slambert@gmail.com>
    
    * archive reports
    
    Signed-off-by: smlambert <slambert@gmail.com>
    
    * Update results dir
    
    Signed-off-by: smlambert <slambert@gmail.com>
    
    * Create testResults dir
    
    Signed-off-by: smlambert <slambert@gmail.com>
    
    * Test Skip core deployment test
    
    Signed-off-by: smlambert <slambert@gmail.com>
    
    * --DargLine="-Djava.util.logging.manager=org.jboss.logmanager.LogManager"
    
    Signed-off-by: Sophia Guo <sophiag@ca.ibm.com>
    
    * Exclude documentation test
    
    Signed-off-by: Sophia Guo <sophiag@ca.ibm.com>
    
    Co-authored-by: Shelley Lambert <smlamber@ca.ibm.com>

---
+++ external/quarkus/dockerfile/quarkus-test.sh
@@ -0,0 +1,53 @@
+#/bin/bash
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Set up Java to be used by the the quarkus-test
+
+if [ -d /java/jre/bin ];then
+	echo "Using mounted Java8"
+	export JAVA_BIN=/java/jre/bin
+	export JAVA_HOME=/java
+	export PATH=$JAVA_BIN:$PATH
+elif [ -d /java/bin ]; then
+	echo "Using mounted Java"
+	export JAVA_BIN=/java/bin
+	export JAVA_HOME=/java
+	export PATH=$JAVA_BIN:$PATH
+else
+	echo "Using docker image default Java"
+	java_path=$(type -p java)
+	suffix="/java"
+	java_root=${java_path%$suffix}
+	export JAVA_BIN="$java_root"
+	echo "JAVA_BIN is: $JAVA_BIN"
+	export JAVA_HOME="${java_root%/bin}"
+fi
+
+java -version
+
+# See https://github.com/quarkusio/quarkus/blob/master/CONTRIBUTING.md#frequently-asked-questions
+# for advise to set MAVEN_OPTS to avoid https://cwiki.apache.org/confluence/display/MAVEN/OutOfMemoryError
+export MAVEN_OPTS="-Xmx1g"
+
+cd /quarkus
+pwd
+echo "Compile and run quarkus tests"
+
+test_exit_code=0
+./mvnw -DargLine="-Djava.util.logging.manager=org.jboss.logmanager.LogManager" -pl '!:quarkus-documentation' clean install
+if [ $? -ne 0 ]; then
+	test_exit_code=$?
+fi
+find ./ -type d -name 'surefire-reports' -exec cp -r "{}" /testResults \;
+exit $test_exit_code
