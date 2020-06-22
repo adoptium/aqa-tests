5a0e600f0867b4b43e8974fecbb6a26199f66547
Piyush Gupta <piyush286@gmail.com>
2020-06-22 15:12:45 -0400

    Ability to Dynamically Set CPU Affinities for Benchmarking
    	• Added an affinity script that can dynamically set CPU affinities according to the platform and machine HW
    		○ This script is well-documented. It lists basic usage info, sample commands, common and platform specific dependencies,
    		○ If some machine doesn't have the affinity tool, this script gracefully handles it. We just output a warning and then continue without using affinity.
    	• Added another script that acts like a wrapper between playlist.xml and affinity script and redirects output of affinity script to /dev/null.
    		○ It uses 2 physical CPUs with SMT as default, which might be the most common perf testing use case. Hence, we can avoid passing those in the playlist.xml file for every test. In case, we do want to run some test with different HW config, then we can specify them only for that test. Also, if you want to run all tests with different affinity than the default, you can easily change this file for one-off testing.
    	• Used affinity command for just one benchmark to begin with
    	• Added a check to avoid downloading the Renaissance benchmark binary if it already exists
    
    Issues:
    https://github.com/AdoptOpenJDK/openjdk-tests/issues/1587
    https://github.com/AdoptOpenJDK/TKG/issues/34
    
    Signed-off-by: Piyush Gupta <piyush286@gmail.com>

---
+++ perf/run_with_affinity.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+################################################################################
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+
+SERVER_PHYSCPU_NUM=2
+SMT=true
+
+while [ -n "$1" ]; do
+	case $1 in
+		--exec_cmd)
+			shift
+			EXEC_CMD=$1
+			;;	
+		--test_root)
+			shift
+			TEST_ROOT=$1
+			;;				
+		--server-physcpu-num)
+			shift
+			SERVER_PHYSCPU_NUM=$1
+			;;
+		--smt)
+			shift
+			SMT=$1
+			;;	
+		*)
+			echo "Unknown parameter: $1" >&2
+			exit 1
+			;;
+	esac
+	shift
+done
+	
+. "${TEST_ROOT}/perf/affinity.sh" > /dev/null 2>&1
+setServerLoadAffinities --server-physcpu-num $SERVER_PHYSCPU_NUM --smt $SMT > /dev/null 2>&1
+
+if [ -z "${SERVER_AFFINITY_CMD}" ]; then
+    echo "Warning!!! Affinity is NOT set. Affinity tool may NOT be installed/supported."
+fi
+   
+EXEC_CMD_WITH_AFFINITY="${SERVER_AFFINITY_CMD} ${EXEC_CMD}"
+echo "Running EXEC_CMD_WITH_AFFINITY=${EXEC_CMD_WITH_AFFINITY}"
+
+${EXEC_CMD_WITH_AFFINITY}
