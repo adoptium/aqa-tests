joeyleeeeeee97/openjdk-tests

Add spring and netty tests

external/common_functions.sh
@@ -30,7 +30,7 @@ supported_packages="jdk jre"
 supported_builds="slim full"
 
 # Supported tests
-supported_tests="camel derby elasticsearch jacoco jenkins functional-test kafka lucene-solr openliberty-mp-tck payara-mp-tck quarkus quarkus_quickstarts scala system-test thorntail-mp-tck tomcat tomee wildfly wycheproof"
+supported_tests="camel derby elasticsearch jacoco jenkins functional-test kafka lucene-solr openliberty-mp-tck payara-mp-tck quarkus quarkus_quickstarts scala system-test thorntail-mp-tck tomcat tomee wildfly wycheproof netty spring"
 
 function check_version() {
     version=$1
@@ -478,6 +478,36 @@ function set_test_info() {
         centos_packages="git wget unzip zip gcc-c++"
         clefos_packages="${centos_packages}"
         ubi_packages="git wget unzip zip gcc-c++"
+        ubi_minimal_packages="${ubi_packages}"
+        ;;
+    netty)
+        github_url="https://github.com/google/wycheproof.git"
+        script="netty-test.sh"
+        home_path="\${WORKDIR}"
+        test_results="testResults"
+        tag_version="4.1"
+        debian_packages="git maven"
+        debianslim_packages="${debian_packages}"
+        ubuntu_packages="${debian_packages}"
+        alpine_packages="bash git maven"
+        centos_packages="git maven"
+        clefos_packages="${centos_packages}"
+        ubi_packages="git maven"
+        ubi_minimal_packages="${ubi_packages}"
+        ;;
+    spring)
+        github_url="https://github.com/spring-projects/spring-boot.git"
+        script="spring-test.sh"
+        home_path="\${WORKDIR}"
+        test_results="testResults"
+        gradle_version="5.1"
+        debian_packages="git wget unzip"
+        debianslim_packages="${debian_packages}"
+        ubuntu_packages="${debian_packages}"
+        alpine_packages="bash git wget unzip"
+        centos_packages="git wget unzip"
+        clefos_packages="${centos_packages}"
+        ubi_packages="git wget unzip"
         ubi_minimal_packages="${ubi_packages}"
         ;;
     *)

external/netty/dockerfile/netty-test.sh
@@ -1,0 +1,48 @@
+#/bin/bash
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#Set up Java to be used by the the example-test
+
+if [ -d /java/jre/bin ];then
+	echo "Using mounted Java8"
+	export JAVA_BIN=/java/jre/bin
+	export JAVA_HOME=/java
+	export PATH=$JAVA_BIN:$PATH
+elif [ -d /java/bin ]; then
+	echo "Using mounted Java9"
+	export JAVA_BIN=/java/bin
+	export JAVA_HOME=/java
+	export PATH=$JAVA_BIN:$PATH
+else
+	echo "Using docker image default Java"
+	java_path=$(type -p java)
+	suffix="/java"
+	java_root=${java_path%$suffix}
+	export JAVA_BIN="$java_root"
+	echo "JAVA_BIN is: $JAVA_BIN"
+	export JAVA_HOME="${java_root%/bin}"
+fi
+
+TEST_SUITE=$1
+java -version
+
+# Replace the following with the initial command lines that trigger execution of your test
+# For this example, we will simply compile and run the ExampleTest class
+pwd
+
+set -e
+cd /netty/
+mvn clean package
+echo "Compile and execute example-test" && \
+set +e

adoptium/aqa-tests

external/common_functions.sh
@@ -30,7 +30,7 @@ supported_packages="jdk jre"
 supported_builds="slim full"
 
 # Supported tests
-supported_tests="camel derby elasticsearch jacoco jenkins functional-test kafka lucene-solr openliberty-mp-tck payara-mp-tck quarkus quarkus_quickstarts scala system-test thorntail-mp-tck tomcat tomee wildfly wycheproof"
+supported_tests="camel derby elasticsearch jacoco jenkins functional-test kafka lucene-solr openliberty-mp-tck payara-mp-tck quarkus quarkus_quickstarts scala system-test thorntail-mp-tck tomcat tomee wildfly wycheproof netty spring"
 
 function check_version() {
     version=$1
@@ -478,6 +478,36 @@ function set_test_info() {
         centos_packages="git wget unzip zip gcc-c++"
         clefos_packages="${centos_packages}"
         ubi_packages="git wget unzip zip gcc-c++"
+        ubi_minimal_packages="${ubi_packages}"
+        ;;
+    netty)
+        github_url="https://github.com/netty/netty.git"
+        script="netty-test.sh"
+        home_path="\${WORKDIR}"
+        test_results="testResults"
+        tag_version="4.1"
+        debian_packages="git maven autoconf automake libtool make tar gcc-multilib libaio-dev"
+        debianslim_packages="${debian_packages}"
+        ubuntu_packages="${debian_packages}"
+        alpine_packages="bash git maven"
+        centos_packages="git maven"
+        clefos_packages="${centos_packages}"
+        ubi_packages="git maven"
+        ubi_minimal_packages="${ubi_packages}"
+        ;;
+    spring)
+        github_url="https://github.com/spring-projects/spring-boot.git"
+        script="spring-test.sh"
+        home_path="\${WORKDIR}"
+        test_results="testResults"
+        gradle_version="5.1"
+        debian_packages="git wget unzip"
+        debianslim_packages="${debian_packages}"
+        ubuntu_packages="${debian_packages}"
+        alpine_packages="bash git wget unzip"
+        centos_packages="git wget unzip"
+        clefos_packages="${centos_packages}"
+        ubi_packages="git wget unzip"
         ubi_minimal_packages="${ubi_packages}"
         ;;
     *)

external/netty/dockerfile/netty-test.sh
@@ -1,0 +1,44 @@
+#/bin/bash
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#Set up Java to be used by the the example-test
+
+if [ -d /java/jre/bin ];then
+	echo "Using mounted Java8"
+	export JAVA_BIN=/java/jre/bin
+	export JAVA_HOME=/java
+	export PATH=$JAVA_BIN:$PATH
+elif [ -d /java/bin ]; then
+	echo "Using mounted Java9"
+	export JAVA_BIN=/java/bin
+	export JAVA_HOME=/java
+	export PATH=$JAVA_BIN:$PATH
+else
+	echo "Using docker image default Java"
+	java_path=$(type -p java)
+	suffix="/java"
+	java_root=${java_path%$suffix}
+	export JAVA_BIN="$java_root"
+	echo "JAVA_BIN is: $JAVA_BIN"
+	export JAVA_HOME="${java_root%/bin}"
+fi
+
+TEST_SUITE=$1
+java -version
+
+set -e
+cd /netty/
+echo "Compile and execute netty-test" && \
+mvn clean package
+set +e