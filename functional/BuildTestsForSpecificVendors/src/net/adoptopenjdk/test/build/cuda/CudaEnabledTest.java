44c8f4d840c8e3b670f1aff20c0e302c87747138
Adam Farley <adfarley@redhat.com>
2021-01-06 11:35:32 +0000

    jjjjjjjjj

--- functional/BuildTestsForSpecificVendors/src/net/adoptopenjdk/test/build/cuda/CudaEnabledTest.java
+++ functional/BuildTestsForSpecificVendors/src/net/adoptopenjdk/test/build/cuda/CudaEnabledTest.java
@@ -1,6 +1,6 @@
 package net.adoptopenjdk.test.build.cuda;
 
-import java.io.BufferedReader
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
