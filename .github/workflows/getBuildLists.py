a934ad5dc5a4e4b8c6a3a93293ac9c54bd4a3920
xichen1 <xichen.pan@gmail.com>
2021-02-08 20:34:59 -0700

    merge openj9 and hotspot, deal with duplication situation

--- .github/workflows/getBuildLists.py
+++ .github/workflows/getBuildLists.py
@@ -5,15 +5,17 @@ import json
 def main():
     print(sys.argv)
     result = []
+    # TODO: handle the change of buildenv dir
     for argument in sys.argv:
-        if (argument.startswith('perf/')):
+        if (argument.startswith('perf/') and ('perf' not in result)):
             result.append('perf')
-        elif (argument.startswith('system/')):
+        elif (argument.startswith('system/') and ('system' not in result)):
             result.append('system')
-        elif (argument.startswith('functional/')):
+        elif (argument.startswith('functional/') and ('functional' not in result)):
             result.append('functional')
-        elif (argument.startswith('openjdk/')):
+        elif (argument.startswith('openjdk/') and ('openjdk' not in result)):
             result.append('openjdk')
+
     if not result:
         result.append('skip')
     print('::set-output name=build_lists::{}'.format(json.dumps(result)))
