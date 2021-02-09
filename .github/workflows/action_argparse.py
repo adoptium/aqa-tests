40e9219f462470be96660fe6110e2f2eae6faef7
Deric Cheung <cheung.deric@gmail.com>
2021-02-08 19:38:36 -0700

    Prefix underscore-less targets with an underscore
    Change the default target `jdk_math` to `_jdk_math` for it to build properly.
    Prefix targets with an underscore if they do not have one.

--- .github/workflows/action_argparse.py
+++ .github/workflows/action_argparse.py
@@ -3,21 +3,32 @@ import json
 import sys
 
 def map_platforms(platforms):
-  """ Takes in a list of platforms and translates Grinder platorms to corresponding GitHub-hosted runners.
-      This function both modifies and returns the 'platforms' argument.
-  """
+    """ Takes in a list of platforms and translates Grinder platorms to corresponding GitHub-hosted runners.
+        This function both modifies and returns the 'platforms' argument.
+    """
+    
+    platform_map = {
+        'x86-64_windows': 'windows-latest',
+        'x86-64_mac': 'mac-latest',
+        'x86-64_linux': 'ubuntu-latest'
+    }
+    
+    for i, platform in enumerate(platforms):
+        if platform in platform_map:
+            platforms[i] = platform_map[platform]
+    
+    return platforms
 
-  platform_map = {
-    'x86-64_windows': 'windows-latest',
-    'x86-64_mac': 'mac-latest',
-    'x86-64_linux': 'ubuntu-latest'
-  }
-
-  for i, platform in enumerate(platforms):
-    if platform in platform_map:
-        platforms[i] = platform_map[platform]
-
-  return platforms
+def underscore_targets(targets):
+    """ Takes in a list of targets and prefixes them with an underscore if they do not have one.
+    """
+    result = []
+    for target in targets:
+        t = target
+        if target[0] != '_':
+            t = '_' + t
+        result.append(t)
+    return result
 
 def main():
 
@@ -33,7 +44,7 @@ def main():
     # Improvement: Automatically resolve the valid choices for each argument populate them below, rather than hard-coding choices.
     parser.add_argument('--sdk_resource', default=['nightly'], choices=['nightly', 'releases'], nargs='+')
     parser.add_argument('--build_list', default=['openjdk'], choices=['openjdk', 'functional', 'system', 'perf', 'external'], nargs='+')
-    parser.add_argument('--target', default=['jdk_math'], nargs='+')
+    parser.add_argument('--target', default=['_jdk_math'], nargs='+')
     parser.add_argument('--platform', default=['x86-64_linux'], nargs='+')
     parser.add_argument('--jdk_version', default=['8'], nargs='+')
     parser.add_argument('--jdk_impl', default=['openj9'], nargs='+')
@@ -42,7 +53,7 @@ def main():
     output = {
       'sdk_resource': args.sdk_resource,
       'build_list': args.build_list,
-      'target': args.target,
+      'target': underscore_targets(args.target),
       'platform': map_platforms(args.platform),
       'jdk_version': args.jdk_version,
       'jdk_impl': args.jdk_impl
