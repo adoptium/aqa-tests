0e869c2e282f28ab7dcade7d5547095ff0a5ba08
SAY-droid427 <sayanimallick0218@gmail.com>
2021-07-14 20:04:21 +0530

    Trial teting test.sh

--- external/external_custom/dockerfile/test.sh
+++ external/external_custom/dockerfile/test.sh
@@ -38,15 +38,15 @@ fi
 
 echo_setup
 
-external_test_repo="$(echo ${EXTERNAL_CUSTOM_REPO} | awk -F'/' '{print $NF}' | sed 's/.git//g')"
-echo ${EXTERNAL_CUSTOM_REPO}
+external_test_repo="$(echo ${env.EXTERNAL_CUSTOM_REPO} | awk -F'/' '{print $NF}' | sed 's/.git//g')"
+echo ${env.EXTERNAL_CUSTOM_REPO}
 echo ${external_test_repo}
-echo ${EXTERNAL_TEST_CMD}
+echo ${env.EXTERNAL_TEST_CMD}
 cd /${external_test_repo}
 set -e
 pwd
 echo "Compile and run external tests"
-${EXTERNAL_TEST_CMD}
+${env.EXTERNAL_TEST_CMD}
 test_exit_code=$?
 echo "Build external_custom completed"
 set +e
