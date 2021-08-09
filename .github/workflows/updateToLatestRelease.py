a896c0483a10d303ee01eb6f78d4e959c9c854fa
SAY-droid427 <sayanimallick0218@gmail.com>
2021-08-09 17:48:52 +0530

    Changed logic to look for all subdirectories in external

--- .github/workflows/updateToLatestRelease.py
+++ .github/workflows/updateToLatestRelease.py
@@ -1,56 +1,68 @@
+import os
+import argparse
 import requests
 import json
 
-filepath=input("Enter the file path: ")
-modified_repos = []
+
+parser = argparse.ArgumentParser()
+parser.add_argument('path',help="Path to the file")
+args = parser.parse_args()
 no_releases = []
-path = "https://api.github.com/repos/"
+dest_path = "https://api.github.com/repos/"
 updateflag = False
 
-with open(filepath) as fhand:
-  line = fhand.readline()
-  cnt = 0
-  is_latest = False
+files=os.listdir(args.path)
+for f in files:
+    filepath=args.path[-8:]+"/"+f+"/test.properties"
+    print(filepath)
+    with open(filepath) as fhand:
+        line = fhand.readline()
+        cnt=0
+        is_latest=False
+        modified_repos = []
 
-  while line:
-    process_line = line.strip()
 
-    if process_line.startswith('github_url="https://github.com/'):
-      request_url = path+process_line[31:-5]+"/releases/latest"
-      print(request_url)
+        while line:
+            process_line = line.strip()
 
-    if process_line.startswith('tag_version'):
-      req = requests.get(request_url)
-      try:
-        tag_version = req.json()['tag_name']
-        is_latest = (tag_version == process_line[13:-1])
+            if process_line.startswith('github_url="https://github.com/'):
+                request_url = dest_path+process_line[31:-5]+"/releases/latest"
+                print(request_url)
 
-        # Checking whether the present tag_version is the latest one
-        if(is_latest):
-          print("This is the latest tag")
-        else:
-          line = 'tag_version="{}"\n'.format(tag_version)
-          print("Tag name updated from {} to {}".format(process_line[13:-1],tag_version))
-          updateflag=True
+            if process_line.startswith('tag_version'):
+                print(process_line)
+                req=requests.get(request_url)
+                print("The data for filepath "+filepath)
+                try:
+                    tag_version = req.json()['tag_name']
+                    print(tag_version == process_line[13:-1])
+                    is_latest = (tag_version == process_line[13:-1])
 
-      except:
-        elem = request_url+"\n"
-        no_releases.append(elem)
+                    if(is_latest):
+                        print("This is the latest tag")
+                    else:
+                        line = 'tag_version="{}"\n'.format(tag_version)
+                        print("tag name updated from {} to {}".format(process_line[13:-1],tag_version))
+                        updateflag=True
+                        
+                        
+                    
+                except:
+                    elem = request_url+"\n"
+                    no_releases.append(elem)
+                    
+            modified_repos.append(line)   
+            line=fhand.readline()
+        fhand.close()
+    if updateflag:
+        out_file=open(filepath,"w")
+        for l in modified_repos:
+            out_file.write(l)
+        out_file.close()
+      
+        
 
-    modified_repos.append(line)
-    line = fhand.readline()
-    cnt += 1
-fhand.close()
-
-# printing the urls for which latest/release donot exist
-print("No latest/release for url:")
-for item in no_releases:
-  print(item)
-
-# Updating the tag_version
-if updateflag:
-  out_file = open(filepath, "w")
-  for l in modified_repos:
-    out_file.write(l)
-  out_file.close()
-
+if no_releases:       
+    print("No releases for the following:")
+    for item in no_releases:
+        print(item)
