570ff263b6058b804c8ec8d7aea53a19228b295a
Sophia Guo <sophia.gwf@gmail.com>
2021-09-20 12:58:57 -0400

    Headless
    Signed-off-by: Sophia Guo <sophia.gwf@gmail.com>

--- openjdk/openjdk.mk
+++ openjdk/openjdk.mk
@@ -90,6 +90,13 @@ JTREG_XML_OPTION = -xml:verify
 JTREG_BASIC_OPTIONS += $(JTREG_XML_OPTION)
 # Add any extra options
 JTREG_BASIC_OPTIONS += $(EXTRA_JTREG_OPTIONS)
+JTREG_KEY_OPTIONS :=
+VMOPTION_HEADLESS :=
+ifneq ($(PLATFORM),x86-64_alpine-linux) 
+	JTREG_KEY_OPTIONS := -k:'!headful'
+	VMOPTION_HEADLESS := -Djava.awt.headless=true
+endif
+JTREG_BASIC_OPTIONS += $(JTREG_KEY_OPTIONS)
 
 ifndef JRE_IMAGE
 	JRE_ROOT := $(TEST_JDK_HOME)
