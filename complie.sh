ae2f38b0fc9b1033693dd0ed41eea153fc11db5e
shiyuxiu <xiushiyu666@gmail.com>
2021-10-07 13:31:06 -0600

    change

--- complie.sh
+++ complie.sh
@@ -6,6 +6,6 @@ usage ()
 if [ $USE_TESTENV_PROPERTIES == true ];then
     while read line; do
         export $line
-    done < testenv.properties
+    done < ./testenv/testenv.properties
     make compile
 fi
