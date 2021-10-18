f9afc20abb7f523c01e73a09d024eb0ea747840e
shiyuxiu <xiushiyu666@gmail.com>
2021-10-18 12:37:49 -0600

    fix make

--- compile.sh
+++ compile.sh
@@ -3,10 +3,13 @@ usage ()
 {
 	echo '                [--USE_TESTENV_PROPERTIES ]: use testenv.properties or not'
 }
+RESOLVED_MAKE = "if [ `uname` = AIX ] || [ `uname` = SunOS ] || [ `uname` = *BSD ]; then MAKE=gmake; else MAKE=make; fi"
+
 if [ $USE_TESTENV_PROPERTIES == true ];then
     while read line; do
         export $line
     done < ./testenv/testenv.properties
 fi
+$RESOLVED_MAKE
 cd ./TKG;
 $MAKE compile
