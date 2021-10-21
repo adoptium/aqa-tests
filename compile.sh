0e70595406f8746b3aed7eb6856a6f66e1a48598
shiyuxiu <xiushiyu666@gmail.com>
2021-10-21 14:21:09 -0600

    ..

--- compile.sh
+++ compile.sh
@@ -2,12 +2,28 @@
 usage ()
 {
 	echo '                [--USE_TESTENV_PROPERTIES ]: use testenv.properties or not'
+    echo '                [--MAKE ]: make command'
+
+    echo '                [--JDK_VERSION ]: JDK Version'
+    echo '                [--JDK_IMPL ]: JDK implementation'
+
 }
 
 if [ $USE_TESTENV_PROPERTIES == true ];then
     while read line; do
         export $line
     done < ./testenv/testenv.properties
+    if [ $JDK_VERSION == "8" ];then
+        if [ $JDK_IMPL == "openj9" ] 
+        then
+            export JDK_REPO = $JDK8_REPO
+            export JDK_BRANCH = $JDK8_BRANCH
+        else
+            export JDK_REPO = $JDK8_OPENJ9_REPO
+            export JDK_BRANCH = $JDK8_OPENJ9_BRANCH
+        fi
+    fi
+
 fi
 cd ./TKG;
-make compile
+$MAKE compile
