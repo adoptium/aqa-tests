33e8d8469c87e9a256650c6abc2b0d893dd0a43f
shiyuxiu <xiushiyu666@gmail.com>
2021-10-22 16:34:04 -0600

    fix

--- compile.sh
+++ compile.sh
@@ -1,35 +1,35 @@
 #!/usr/bin/env bash
-usage ()
-{
-	echo '                [--USE_TESTENV_PROPERTIES ]: use testenv.properties or not'
-    echo '                [--JDK_VERSION ]: JDK Version'
-    echo '                [--JDK_IMPL ]: JDK implementation'
+usage() {
+    echo '                Please export USE_TESTENV_PROPERTIES, JDK_VERSION and JDK_IMPL before running the script locally.'
 
 }
-if [ `uname` = AIX ] || [ `uname` = SunOS ] || [ `uname` = *BSD ]; then MAKE=gmake; else MAKE=make; fi
-if [ $USE_TESTENV_PROPERTIES == true ];then
+if [ $(uname) = AIX ] || [ $(uname) = SunOS ] || [ $(uname) = *BSD ]; then
+    MAKE=gmake
+else
+    MAKE=make
+fi
+if [ $USE_TESTENV_PROPERTIES == true ]; then
     while read line; do
         export $line
-    done < ./testenv/testenv.properties
-    openj9_repo=JDK${JDK_VERSION}_OPENJ9_REPO
-    openj9_branch=JDK${JDK_VERSION}_OPENJ9_BRANCH
+    done <./testenv/testenv.properties
+    repo_j9=JDK${JDK_VERSION}_OPENJ9_REPO
+    branch_j9=JDK${JDK_VERSION}_OPENJ9_BRANCH
     repo=JDK${JDK_VERSION}_REPO
     branch=JDK${JDK_VERSION}_BRANCH
-    eval openj9_repo2='$'$openj9_repo
-    eval openj9_branch2='$'$openj9_branch
+    eval repo_j9_2='$'$repo_j9
+    eval branch_j9_2='$'$branch_j9
     eval repo2='$'$repo
     eval branch2='$'$branch
 
-    if [ $JDK_IMPL == "openj9" ]
-    then
-        export JDK_REPO=$openj9_repo2
-        export JDK_BRANCH=$openj9_branch2
-        
+    if [ $JDK_IMPL == "openj9" ]; then
+        export JDK_REPO=$repo_j9_2
+        export JDK_BRANCH=$branch_j9_2
+
     else
-        export eval JDK_REPO=$repo2
-        export eval JDK_BRANCH=$branch2
+        export JDK_REPO=$repo2
+        export JDK_BRANCH=$branch2
     fi
 
 fi
-cd ./TKG;
+cd ./TKG
 $MAKE compile
