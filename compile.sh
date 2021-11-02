262c648c8d5d37e7a346431d46987134c8115176
shiyuxiu <xiushiyu666@gmail.com>
2021-11-02 15:29:34 -0600

    change

--- compile.sh
+++ compile.sh
@@ -12,23 +12,19 @@ if [ $USE_TESTENV_PROPERTIES == true ]; then
     while read line; do
         export $line
     done <./testenv/testenv.properties
-    repo_j9=JDK${JDK_VERSION}_OPENJ9_REPO
-    branch_j9=JDK${JDK_VERSION}_OPENJ9_BRANCH
-    repo=JDK${JDK_VERSION}_REPO
-    branch=JDK${JDK_VERSION}_BRANCH
-    eval repo_j9_2='$'$repo_j9
-    eval branch_j9_2='$'$branch_j9
+    if [ $JDK_IMPL == "openj9" ]; then
+        repo=JDK${JDK_VERSION}_OPENJ9_REPO
+        branch=JDK${JDK_VERSION}_OPENJ9_BRANCH
+    else
+        repo=JDK${JDK_VERSION}_REPO
+        branch=JDK${JDK_VERSION}_BRANCH
+    fi
+
     eval repo2='$'$repo
     eval branch2='$'$branch
 
-    if [ $JDK_IMPL == "openj9" ]; then
-        export JDK_REPO=$repo_j9_2
-        export JDK_BRANCH=$branch_j9_2
-
-    else
-        export JDK_REPO=$repo2
-        export JDK_BRANCH=$branch2
-    fi
+    export JDK_REPO=$repo2
+    export JDK_BRANCH=$branch2
     echo "Set values based on ./testenv/testenv.properties:"
     cat ./testenv/testenv.properties
     echo "JDK_REPO=${JDK_REPO}"
