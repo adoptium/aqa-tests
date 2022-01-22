6e691183dfae7d5b7b30c374d6a2674ee697bf76
amir-waheed <amir2@ualberta.ca>
2022-01-22 15:37:08 -0700

    adding node
    Signed-off-by: amir-waheed <amir2@ualberta.ca>

--- buildenv/jenkins/triggerGrinder.groovy
+++ buildenv/jenkins/triggerGrinder.groovy
@@ -1,39 +1,41 @@
 #!groovy
-stage('readJSON')
-{
-  // def json_path = "disabledTestParser/output.json"
-  def json = readJSON file: "disabledTestParser/output.json"
-}
-/**
-  This runs when we have found a job w/ git_issue_status = closed.
-  Collects all parameters and runs grinder
-  */
-def run_grinder(map) {
-  stage("run_grinder")
+node {
+  stage('readJSON')
   {
-    def childParams = []
-    def queryString
-
-    map.each { k, v ->
-      if (k!="ISSUE_TRACKER_STATUS" && k!="ISSUE_TRACKER") {
-          childParams << string(name: "${k}", value: "${v}")
-      }
-    }
-
-    for (e in childparams) {
-      println e
-    }
-
-    build job: Grinder, parameters: childParams, propagate: false
+    // def json_path = "disabledTestParser/output.json"
+    def json = readJSON file: "disabledTestParser/output.json"
   }
-}
+  /**
+    This runs when we have found a job w/ git_issue_status = closed.
+    Collects all parameters and runs grinder
+    */
+  def run_grinder(map) {
+    stage("run_grinder")
+    {
+      def childParams = []
+      def queryString
 
-/**
-Get each of the key-value pairs for each json value.
-If the issue for a job is closed, run grinder on it.
-Otherwise ignore it */
-json.eachWithIndex { dict, _ ->
-  if (dict["GIT_ISSUE_STATUS"] == "closed") {
-    run_grinder(dict)
+      map.each { k, v ->
+        if (k!="ISSUE_TRACKER_STATUS" && k!="ISSUE_TRACKER") {
+            childParams << string(name: "${k}", value: "${v}")
+        }
+      }
+
+      for (e in childparams) {
+        println e
+      }
+
+      build job: Grinder, parameters: childParams, propagate: false
+    }
+  }
+
+  /**
+  Get each of the key-value pairs for each json value.
+  If the issue for a job is closed, run grinder on it.
+  Otherwise ignore it */
+  json.eachWithIndex { dict, _ ->
+    if (dict["GIT_ISSUE_STATUS"] == "closed") {
+      run_grinder(dict)
+    }
   }
 }
