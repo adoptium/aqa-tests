532564d2fa4c8b815fee6a58eb4a2a4d2066bf40
amir-waheed <amir2@ualberta.ca>
2022-01-22 15:19:10 -0700

    fixing stages alignment
    Signed-off-by: amir-waheed <amir2@ualberta.ca>

--- buildenv/jenkins/triggerGrinder.groovy
+++ buildenv/jenkins/triggerGrinder.groovy
@@ -1,32 +1,8 @@
 #!groovy
-stage('triggerGrinder')
+stage('read file and run grinder')
 {
   def json_path = "disabledTestParser/output.json"
   def json = readJSON file: json_path
-
-  /**
-  This runs when we have found a job w/ git_issue_status = closed.
-  Collects all parameters and runs grinder
-  */
-  def run_grinder(map) {
-    stage("run_grinder")
-    {
-      def childParams = []
-      def queryString
-
-      map.each { k, v ->
-        if (k!="ISSUE_TRACKER_STATUS" && k!="ISSUE_TRACKER") {
-            childParams << string(name: "${k}", value: "${v}")
-        }
-      }
-
-      for (e in childparams) {
-        println e
-      }
-
-      build job: Grinder, parameters: childParams, propagate: false
-    }
-  }
 
   /**
   Get each of the key-value pairs for each json value.
@@ -36,5 +12,29 @@ stage('triggerGrinder')
     if (dict["GIT_ISSUE_STATUS"] == "closed") {
       run_grinder(dict)
     }
+  }
+}
+
+/**
+  This runs when we have found a job w/ git_issue_status = closed.
+  Collects all parameters and runs grinder
+  */
+def run_grinder(map) {
+  stage("run_grinder")
+  {
+    def childParams = []
+    def queryString
+
+    map.each { k, v ->
+      if (k!="ISSUE_TRACKER_STATUS" && k!="ISSUE_TRACKER") {
+          childParams << string(name: "${k}", value: "${v}")
+      }
+    }
+
+    for (e in childparams) {
+      println e
+    }
+
+    build job: Grinder, parameters: childParams, propagate: false
   }
 }
