3d1c9f0d5c3055f2b921447302061bb0513169d4
amir-waheed <amir2@ualberta.ca>
2022-01-22 12:24:36 -0700

    Fixing triggerGrinder.groovy. The tests are now started without the use of URL. Fixes adoptium/aqa-tests #2702. Signed-off-by: Abeer Waheed amir2@ualberta.ca
    Signed-off-by: amir-waheed <amir2@ualberta.ca>

--- buildenv/jenkins/triggerGrinder.groovy
+++ buildenv/jenkins/triggerGrinder.groovy
@@ -1,38 +1,27 @@
 #!groovy
 
-def json_path = args.size() ? args[0] : "disabledTestParser/output.json."
+def json_path = args.size() ? args[0] : "disabledTestParser/output.json"
 def json = readJSON file: json_path
-def jenkins_url = "https://ci.adoptopenjdk.net/job/Grinder/buildWithParameters"
 
 /**
  This runs when we have found a job w/ git_issue_status = closed.
  Collects all parameters and runs grinder
  */
 def run_grinder(map) {
-  def list = []
+  def childParams = []
   def queryString
 
   map.each { k, v ->
-    list.add(k + "=" + v)
+    if (k!="ISSUE_TRACKER_STATUS" && k!="ISSUE_TRACKER") {
+        childParams << string(name: "${k}", value: "${v}")
+    }
   }
 
-  queryString = list.join("&")
-  url = jenkins_url + "/?" + queryString
+  for (e in childparams) {
+    println e
+  }
 
-  // The Grinder trigger will look something like this,
-  // but it requires tokens which I do not have yet.
-  // def url = new URL(jenkins_url + "/?" + queryString)
-  // def connection = url.openConnection()
-  // connection.with {
-  //     doOutput = true
-  //     requestMethod = 'POST'
-  //     outputStream.withWriter { writer ->
-  //         writer << queryString
-  //     }
-  //     println content.text
-  // }
-
-  println url
+  build job: Grinder, parameters: childParams, propagate: false
 }
 
 /**
