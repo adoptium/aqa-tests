234d571919ea9d1881ade81f5d74f08dd1346222
amir-waheed <amir2@ualberta.ca>
2022-01-22 16:36:28 -0700

    fixing filepath
    Signed-off-by: amir-waheed <amir2@ualberta.ca>

--- buildenv/jenkins/triggerGrinder.groovy
+++ buildenv/jenkins/triggerGrinder.groovy
@@ -2,7 +2,7 @@
 node {
   stage('readJSON')
   {
-    def json_path = "${WORKSPACE}\\aqa-tests\\disabledTestParser\\output.json"
+    def json_path = "${WORKSPACE}/aqa-tests/disabledTestParser/output.json"
     def json = readJSON file: json_path
   }
   /**
