1eb0b1bac4dec1d48e17d32f288bf76edf77c277
George Adams <georgeadams1995@gmail.com>
2022-01-24 14:56:35 +0000

    re-add Solaris support (#3276)
    * re-add Solaris support
    
    * fix natives path

--- jck/jtrunner/JavaTestRunner.java
+++ jck/jtrunner/JavaTestRunner.java
@@ -230,6 +230,10 @@ public class JavaTestRunner {
 		jtliteJarFullPath = jckBase + File.separator + "lib" + File.separator + "jtlite.jar"; 
 		classesFullPath = jckBase + File.separator + "classes";
 		nativesLoc = jckRoot + File.separator + "natives" + File.separator + platform;
+		// Solaris natives are in /natives/sunos
+		if (platform.equals("solaris")) {
+			nativesLoc = jckRoot + File.separator + "natives" + File.separator + "sunos";
+		}
 		jtiFile = testRoot + File.separator + "jck" + File.separator + "jtrunner" + File.separator + CONFIG + File.separator + jckVersion + File.separator + testSuite.toLowerCase() + ".jti"; 
 		fileUrl = "file:///" + jckBase + "/testsuite.jtt";
 
@@ -488,6 +492,9 @@ public class JavaTestRunner {
 				robotAvailable = "No";
 			} else if (platform.contains("osx")) {
 				libPath = "DYLD_LIBRARY_PATH";
+				robotAvailable = "Yes";
+			} else if (platform.contains("solaris")) {
+				libPath = "LD_LIBRARY_PATH";
 				robotAvailable = "Yes";
 			} else {
 				System.out.println("Unknown platform:: " + platform);
@@ -754,7 +761,7 @@ public class JavaTestRunner {
 				jxcCmd = jckBase + File.separator + "macos" + File.separator + "bin" + File.separator + "schemagen.sh";
 				genCmd = jckBase + File.separator + "macos" + File.separator + "bin" + File.separator + "wsgen.sh";
 				impCmd = jckBase + File.separator + "macos" + File.separator + "bin" + File.separator + "wsimport.sh";
-			} else if (platform.equals("zos")) {
+			} else if (platform.equals("zos") || platform.equals("solaris")) {
 				pathToJavac = testJdk + File.separator + "bin" + File.separator + "javac";
 				xjcCmd = jckBase + File.separator + "solaris" + File.separator + "bin" + File.separator + "xjc.sh";
 				jxcCmd = jckBase + File.separator + "solaris" + File.separator + "bin" + File.separator + "schemagen.sh";
