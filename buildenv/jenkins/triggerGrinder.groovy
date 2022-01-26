fb8e25eaf27aab8edb201f2546fd36b24911e6a7
amir-waheed <amir2@ualberta.ca>
2022-01-25 19:14:05 -0700

    Editing scm checkout step
    Signed-off-by: amir-waheed <amir2@ualberta.ca>

--- buildenv/jenkins/triggerGrinder.groovy
+++ buildenv/jenkins/triggerGrinder.groovy
@@ -14,14 +14,7 @@ node {
       run_grinder(dict)
     }
   }
-  checkout scm: [$class: 'GitSCM',
-                            branches: [[name: "${scm.branches[0].name}"]],
-                            extensions: [
-                                [$class: 'CleanBeforeCheckout'],
-                                [$class: 'CloneOption', reference: ref_cache],
-                                [$class: 'RelativeTargetDirectory', relativeTargetDir: 'aqa-tests']],
-                            userRemoteConfigs: [[url: "${gitConfig.getUrl()}"]]
-                        ]
+  checkout scm: [$class: 'GitSCM']
 }
 /**
   This runs when we have found a job w/ git_issue_status = closed.
