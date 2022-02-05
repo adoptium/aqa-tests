484a33210d30633ed4933be7d11b0801f303a778
amir-waheed <amir2@ualberta.ca>
2022-02-05 03:24:06 -0700

    Cleaning the code
    Signed-off-by: amir-waheed <amir2@ualberta.ca>

--- buildenv/jenkins/triggerGrinder.groovy
+++ buildenv/jenkins/triggerGrinder.groovy
@@ -1,49 +1,47 @@
 #!groovy
+
+List<Map<String, Object>> json = []
+
 node {
-   checkout scm
-  //  : [$class: 'GitSCM',
-  //                           branches: [[name: "${scm.branches[0].name}"]],
-  //                           extensions: [
-  //                               [$class: 'CleanBeforeCheckout'],
-  //                               [$class: 'CloneOption', reference: ref_cache],
-  //                               [$class: 'RelativeTargetDirectory', relativeTargetDir: 'aqa-tests']],
-  //                           userRemoteConfigs: [[url: "${gitConfig.getUrl()}"]]
-                        // ]
-  stage('readJSON')
-  {
+  checkout scm
+  stage('readJSON') {
     def json_path = "${WORKSPACE}/aqa-tests/disabledTestParser/output.json"
-    def json = readJSON file: json_path
-    /**
-    Get each of the key-value pairs for each json value.
-    If the issue for a job is closed, run grinder on it.
-    Otherwise ignore it */
-    json.eachWithIndex { dict, _ ->
-      if (dict["GIT_ISSUE_STATUS"] == "closed") {
-        run_grinder(dict)
-      }
+    json = readJSON(file: json_path) as List<Map<String, Object>>
+  }
+  stage('Launch Grinder Jobs') {
+    launch_grinders(json)
+  }
+}
+
+def launch_grinders(List<Map<String, Object>> json) {
+  /**
+   Get each of the key-value pairs for each json value.
+   If the issue for a job is closed, run grinder on it.
+   Otherwise ignore it
+   */
+  json.eachWithIndex { dict, _ ->
+    if (dict["ISSUE_TRACKER_STATUS"] == "closed") {
+      run_grinder(dict)
     }
   }
 }
+
 /**
-  This runs when we have found a job w/ git_issue_status = closed.
-  Collects all parameters and runs grinder
-  */
-def run_grinder(map) {
-  stage("running grinder")
-  {
-    def childParams = []
-    def queryString
+ This runs when we have found a job w/ git_issue_status = closed.
+ Collects all parameters and runs grinder
+ */
+def run_grinder(Map<String, Object> map) {
+  def childParams = []
 
-    map.each { k, v ->
-      if (k!="ISSUE_TRACKER_STATUS" && k!="ISSUE_TRACKER") {
-          childParams << string(name: "${k}", value: "${v}")
-      }
+  map.each { k, v ->
+    if (k != "ISSUE_TRACKER_STATUS" && k != "ISSUE_TRACKER") {
+      childParams << string(name: "${k}", value: "${v}")
     }
-
-    for (e in childparams) {
-      println e
-    }
-
-    build job: Grinder, parameters: childParams, propagate: false
   }
+
+  for (e in childParams) {
+    println e
+  }
+
+  build job: "Grinder", parameters: childParams, propagate: false
 }
