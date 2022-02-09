377e2a7aab536427b805a7e446e7e8673d38185f
amir-waheed <77027937+amir-waheed@users.noreply.github.com>
2022-02-09 16:49:58 -0700

    Fixing triggerGrinder.groovy.  (#3268)
    * Fixing triggerGrinder.groovy. The tests are now started without the use of URL. Fixes adoptium/aqa-tests #2702. 
    
    * Fixing json_path format
    
    * Small change in syntax of readJson
    
    * change is file path
    
    * adding stages
    
    * fixing stages alignment
    
    * adding steps
    
    * adding missing bracket
    
    * removing steps
    
    * adding node
    
    * rearranging function def
    
    * changing filepath for output.json
    
    * added workspace variable
    
    Signed-off-by: amir-waheed <amir2@ualberta.ca>
    
    * fixing workspace in filepath
    
    * fixing filepath
    
    * Adding scm checkout steps
    
    * Editing scm checkout step
    
    * fixing scm checkout
    
    * editing the scm step
    
    * adding stage back to function
    
    * Cleaning the code
    
    * Getting result of the job
    
    * variable isSuccess is the status of the job
    
    * Setting propagate to true
    
    Signed-off-by: amir-waheed <amir2@ualberta.ca>

--- buildenv/jenkins/triggerGrinder.groovy
+++ buildenv/jenkins/triggerGrinder.groovy
@@ -1,46 +1,47 @@
 #!groovy
 
-def json_path = args.size() ? args[0] : "disabledTestParser/output.json."
-def json = readJSON file: json_path
-def jenkins_url = "https://ci.adoptopenjdk.net/job/Grinder/buildWithParameters"
+List<Map<String, Object>> json = []
+
+node {
+  checkout scm
+  stage('readJSON') {
+    def json_path = "${WORKSPACE}/aqa-tests/disabledTestParser/output.json"
+    json = readJSON(file: json_path) as List<Map<String, Object>>
+  }
+  stage('Launch Grinder Jobs') {
+    launch_grinders(json)
+  }
+}
+
+def launch_grinders(List<Map<String, Object>> json) {
+  /**
+   Get each of the key-value pairs for each json value.
+   If the issue for a job is closed, run grinder on it.
+   Otherwise ignore it
+   */
+  json.eachWithIndex { dict, _ ->
+    if (dict["ISSUE_TRACKER_STATUS"] == "closed") {
+      run_grinder(dict)
+    }
+  }
+}
 
 /**
  This runs when we have found a job w/ git_issue_status = closed.
  Collects all parameters and runs grinder
  */
-def run_grinder(map) {
-  def list = []
-  def queryString
+def run_grinder(Map<String, Object> map) {
+  def childParams = []
 
   map.each { k, v ->
-    list.add(k + "=" + v)
+    if (k != "ISSUE_TRACKER_STATUS" && k != "ISSUE_TRACKER") {
+      childParams << string(name: "${k}", value: "${v}")
+    }
   }
 
-  queryString = list.join("&")
-  url = jenkins_url + "/?" + queryString
-
-  // The Grinder trigger will look something like this,
-  // but it requires tokens which I do not have yet.
-  // def url = new URL(jenkins_url + "/?" + queryString)
-  // def connection = url.openConnection()
-  // connection.with {
-  //     doOutput = true
-  //     requestMethod = 'POST'
-  //     outputStream.withWriter { writer ->
-  //         writer << queryString
-  //     }
-  //     println content.text
-  // }
-
-  println url
-}
-
-/**
- Get each of the key-value pairs for each json value.
- If the issue for a job is closed, run grinder on it.
- Otherwise ignore it */
-json.eachWithIndex { dict, _ ->
-  if (dict["GIT_ISSUE_STATUS"] == "closed") {
-    run_grinder(dict)
+  for (e in childParams) {
+    println e
   }
+
+  def job = build job: "Grinder", parameters: childParams, propagate: true
 }
