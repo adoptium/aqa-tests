912cc3db3c2ebbdf72814b1b2597ba182e8747ee
amir-waheed <amir2@ualberta.ca>
2022-02-11 18:49:58 -0700

    Fixing semantic error 2
    Signed-off-by: amir-waheed <amir2@ualberta.ca>

--- buildenv/jenkins/triggerGrinder.groovy
+++ buildenv/jenkins/triggerGrinder.groovy
@@ -12,18 +12,21 @@ node {
 
 stage('Launch Grinder Jobs')
 {
+  launch_grinders(json)
+}
+
+def launch_grinders(List<Map<String, Object>> json) {
+  /**
+   Get the parameters specified as a string list.
+   Get each of the key-value pairs for each json value.
+   If the issue for a job is closed, and JDK_VERSION and 
+   JDK_IMPL match the parameters specified, 
+   run grinder on it. Otherwise ignore it
+   */
+
   def jdk_ver = params.JDK_VERSION.split(',')
   def jdk_imp =  params.JDK_IMPL.split(',')
 
-  launch_grinders(json, jdk_ver, jdk_imp)
-}
-
-def launch_grinders(List<Map<String, Object>> json, String[] jdk_ver, String[] jdk_imp) {
-  /**
-   Get each of the key-value pairs for each json value.
-   If the issue for a job is closed, run grinder on it.
-   Otherwise ignore it
-   */
   json.eachWithIndex { dict, _ ->
     if (dict["ISSUE_TRACKER_STATUS"] == "closed" && jdk_ver.contains(dict["JDK_VERSION"].toString()) && jdk_imp.contains(dict["JDK_IMPL"])) {
       run_grinder(dict)
