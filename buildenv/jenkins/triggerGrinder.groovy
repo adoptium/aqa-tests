8655f2108e28adc662849a9b63e92e5b2cc49ca6
amir-waheed <77027937+amir-waheed@users.noreply.github.com>
2022-02-24 08:28:57 -0700

    Update triggerGrinder.groovy file (#3346)
    * Update triggerGrinder.groovy file. Moving launch_grinders outside the node.
    
    * Adding a stage for launch grinder
    
    * Printing the parameters values
    
    * Seperating the values at commas
    
    * Filtering for jdk_versin and jdk_impl
    
    * Fixing syntax error
    
    * Fixing semantic error
    
    * Adding logic to trigger python scripts and removing output.json file
    
    * Changing the python file name
    
    * Delete ProblemList.json
    
    * Delete generateDisabledTestListJson.cpython-39.pyc
    
    Signed-off-by: amir-waheed <amir2@ualberta.ca>

--- buildenv/jenkins/triggerGrinder.groovy
+++ buildenv/jenkins/triggerGrinder.groovy
@@ -4,23 +4,36 @@ List<Map<String, Object>> json = []
 
 node {
   checkout scm
+  stage('Triggering python script')
+  {
+    sh "python ${WORKSPACE}/aqa-tests/disabledTestParser/generateDisabledTestListJson.py"
+    sh "python ${WORKSPACE}/aqa-tests/disabledTestParser/issue_status.py"
+  }
   stage('readJSON') {
     def json_path = "${WORKSPACE}/aqa-tests/disabledTestParser/output.json"
     json = readJSON(file: json_path) as List<Map<String, Object>>
   }
-  stage('Launch Grinder Jobs') {
-    launch_grinders(json)
-  }
+}
+
+stage('Launch Grinder Jobs')
+{
+  launch_grinders(json)
 }
 
 def launch_grinders(List<Map<String, Object>> json) {
   /**
+   Get the parameters specified as a string list.
    Get each of the key-value pairs for each json value.
-   If the issue for a job is closed, run grinder on it.
-   Otherwise ignore it
+   If the issue for a job is closed, and JDK_VERSION and 
+   JDK_IMPL match the parameters specified, 
+   run grinder on it. Otherwise ignore it
    */
+
+  def jdk_ver = params.JDK_VERSION.split(',')
+  def jdk_imp =  params.JDK_IMPL.split(',')
+
   json.eachWithIndex { dict, _ ->
-    if (dict["ISSUE_TRACKER_STATUS"] == "closed") {
+    if (dict["ISSUE_TRACKER_STATUS"] == "closed" && jdk_ver.contains(dict["JDK_VERSION"].toString()) && jdk_imp.contains(dict["JDK_IMPL"])) {
       run_grinder(dict)
     }
   }
