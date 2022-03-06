1df2bb4bdde5789e82cb17e79d5b230cd22a3edc
gervaisj <gervaisj14@outlook.com>
2022-03-06 17:58:54 -0500

    Use token, introduce command line options, refactor

--- scripts/disabled_tests/issue_status.py
+++ scripts/disabled_tests/issue_status.py
@@ -1,40 +1,259 @@
+import abc
+import argparse
+import enum
 import json
+import logging
+import os
+import sys
+import urllib.parse
+from collections import defaultdict
+from concurrent import futures
+from typing import List, Dict
+
 import requests
+import requests.auth
+
+from common import models
+
+logging.basicConfig(
+    format="%(levelname)s - %(message)s",
+)
+LOG = logging.getLogger()
+
+GITHUB_USER_ENV = "AQA_ISSUE_TRACKER_GITHUB_USER"
+GITHUB_TOKEN_ENV = "AQA_ISSUE_TRACKER_GITHUB_TOKEN"
 
 
-def json_parser(json_obj):
-    jsonLoad = json.loads(json_obj)
-    return jsonLoad["ISSUE_TRACKER"]
+class Status(enum.Enum):
+    OPEN = ('open',)
+    CLOSED = ('closed',)
 
-def find_state(query_url):
-    params = {'accept': 'application/vnd.github.v3+json',
-              'state': 'all'}
+    def __init__(self, scheme_name):
+        self.scheme_name = scheme_name
+
+
+def _extract_path(url):
+    """
+    Extracts the URL path, including the root slash
+
+    e.g. extracts '/3/library/urllib.parse.html'
+    from 'https://docs.python.org:80/3/library/urllib.parse.html'
+    """
+    path = urllib.parse.urlparse(url).path
+    return path
+
+
+class HandlerException(Exception):
+    pass
+
+
+class NoHandlerFoundException(Exception):
+    pass
+
+
+class BaseHandler(abc.ABC):
+    """
+    Fetches the status of an issue given its tracking URL
+    """
+    STATUS_NAME_TO_ENUM = {
+        'open': Status.OPEN,
+        'closed': Status.CLOSED,
+        'resolved': Status.CLOSED,
+    }
+
+    @classmethod
+    def name_to_status(cls, name):
+        maybe_status = cls.STATUS_NAME_TO_ENUM.get(name)
+        if maybe_status is None:
+            raise HandlerException(f"Unrecognized issue status name: {name!r}")
+        return maybe_status
+
+    @abc.abstractmethod
+    def can_handle(self, url) -> bool:
+        pass
+
+    @abc.abstractmethod
+    def handle(self, url) -> Status:
+        pass
+
+
+class GitHubHandler(BaseHandler):
+    """
+    URL handler for GitHub
+    """
+    GITHUB_API_BASE_URL = f'https://api.github.com/repos'
+    PARAMS = {'accept': 'application/vnd.github.v3+json', 'state': 'all'}
+
+    def __init__(self, user=None, token=None):
+        self.user = user
+        self.token = token
+
+    def can_handle(self, url):
+        return 'github.com' in url
+
+    def handle(self, url):
+        path = _extract_path(url)
+        url = self.GITHUB_API_BASE_URL + path
+
+        # Use anonymous auth if user/token not provided
+        if all([self.user, self.token]):
+            auth = requests.auth.HTTPBasicAuth(self.user, self.token)
+        else:
+            auth = None
+        resp = requests.get(url, params=self.PARAMS, auth=auth)
+        resp.raise_for_status()
+        resp_json = resp.json()
+        status_name = resp_json["state"]
+        return self.name_to_status(status_name)
+
+
+class BugsOpenJdkHandler(BaseHandler):
+    """
+    URL handler for bugs.openjdk (Jira-based board)
+    """
+    BUGS_OPENJDK_API_BASE_URL = f'https://bugs.openjdk.java.net/rest/api/latest/issue'
+
+    def can_handle(self, url):
+        return 'bugs.openjdk' in url
+
+    def handle(self, url):
+        path = _extract_path(url)
+        *_, issue_key = path.split('/')  # get the element after the last slash
+        url = self.BUGS_OPENJDK_API_BASE_URL + '/' + issue_key
+
+        resp = requests.get(url)
+        resp.raise_for_status()
+        resp_json = resp.json()
+        status_name = resp_json.get('fields', {}).get('status', {}).get('name', '').lower()
+        return self.name_to_status(status_name)
+
+
+class Dispatcher:
+    """
+    Dispatches each issue tracker to its respective handler
+    """
+
+    def __init__(self, handlers: List[BaseHandler]):
+        self.handlers = handlers
+
+    def dispatch(self, issue_url: str):
+        for hdl in self.handlers:
+            if hdl.can_handle(issue_url):
+                return hdl.handle(issue_url)
+
+        raise NoHandlerFoundException()
+
+
+def augment_with_status(issues, issue_status):
+    """
+    Augment all issue items with the provided status
+    """
+    issues_with_status = [
+        models.SchemeWithStatus(
+            **issue,
+            ISSUE_TRACKER_STATUS=issue_status.scheme_name,
+        )
+        for issue in issues
+    ]
+    return issues_with_status
+
+
+def group_issues_by_url(issues: List[models.Scheme]) -> Dict[str, List[models.Scheme]]:
+    url_to_issues = defaultdict(list)
+    for issue in issues:
+        url_to_issues[issue["ISSUE_TRACKER"]].append(issue)
+    return url_to_issues
+
+
+def _handle_completed_future(future, log_prefix, url, url_to_issues):
     try:
-        response = requests.get(query_url, params=params).json()
-        json_str = json.dumps(response)
-        return json.loads(json_str)["state"]
-    except:
-        print("An exception occurred! Url is invalid")
+        issue_status: Status = future.result()
+    except HandlerException as he:
+        LOG.error(f"{log_prefix} Error when handling {url!r}: {he}")
+    except NoHandlerFoundException:
+        LOG.error(f"{log_prefix} No handler found for {url!r}")
+    except Exception as e:
+        LOG.error(f"{log_prefix} Uncaught exception for {url!r}: {e}")
+    else:
+        LOG.info(f"{log_prefix} Ended processing for {url!r}: {issue_status.name}")
+        issues_with_status = augment_with_status(url_to_issues[url], issue_status)
+        return issues_with_status
+    # return an empty list if an error was caught
+    return []
+
+
+def fetch_all_statuses(issues: List[models.Scheme], dispatcher: Dispatcher, max_workers):
+    """
+    Fetch the status of all issue trackers using the provided dispatcher
+    and augment each issue with their respective status
+    """
+    url_to_issues = group_issues_by_url(issues)
+    len_trackers = len(url_to_issues)
+    LOG.debug(f"Unique issue trackers found: {len_trackers}")
+
+    # When `max_workers` is None, `ThreadPoolExecutor` uses a sensible default value based on the number of cores
+    all_issues = []
+    with futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+        future_to_url = {executor.submit(dispatcher.dispatch, url): url for url in url_to_issues}
+        for i, future in enumerate(futures.as_completed(future_to_url)):
+            url = future_to_url[future]
+            log_prefix = f"{i + 1}/{len_trackers} -"
+            issues_with_status = _handle_completed_future(future, log_prefix, url, url_to_issues)
+            all_issues.extend(issues_with_status)
+    return all_issues
+
 
 def main():
-    base_url = f'https://api.github.com/repos/'
-    f = open('ProblemList.json',)
-    data = json.load(f)
-    
-    for j in data:
-        str_json = json.dumps(j)
-        git_issue_url = json_parser(str_json)
-        repo_issueUrl = git_issue_url.replace("https://github.com/", "")
-        final_url = base_url + repo_issueUrl
-        state = find_state(final_url)
-        j["ISSUE_TRACKER_STATUS"] = state  
-    
-    with open('output.json', 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
+    parser = argparse.ArgumentParser(description="Fetch issue status for each disabled test", allow_abbrev=False)
+    parser.add_argument('--github-user', metavar='USER',
+                        help=f"GitHub User used for API [env: {GITHUB_USER_ENV}]")
+    parser.add_argument('--github-token', metavar='TOKEN',
+                        help=f"GitHub Token used for API [env: {GITHUB_TOKEN_ENV}]")
+    parser.add_argument('--max-workers', type=int, default=None,
+                        help=f"Maximum number of threads to spawn, default determined by ThreadPoolExecutor")
+    parser.add_argument('--infile', '-i', type=argparse.FileType('r'), default=sys.stdin,
+                        help='Input file, defaults to stdin')
+    parser.add_argument('--outfile', '-o', type=argparse.FileType('w'), default=sys.stdout,
+                        help='Output file, defaults to stdout')
+    parser.add_argument('--verbose', '-v', action='count', default=0,
+                        help="Enable info logging level, debug level if -vv")
+    args = parser.parse_args()
+
+    if not args.github_user:
+        args.github_user = os.getenv(GITHUB_USER_ENV)
+        if not args.github_user:
+            parser.print_usage()
+            parser.exit(1, message="GitHub User must be provided as environment variable or command-line argument")
+
+    if not args.github_token:
+        args.github_token = os.getenv(GITHUB_TOKEN_ENV)
+        if not args.github_token:
+            parser.print_usage()
+            parser.exit(1, message="GitHub Token must be provided as environment variable or command-line argument")
+
+    if args.verbose == 1:
+        LOG.setLevel(logging.INFO)
+    elif args.verbose == 2:
+        LOG.setLevel(logging.DEBUG)
+
+    LOG.debug(f"Loading JSON from {getattr(args.infile, 'name', '<unknown>')}")
+    issues: List[models.Scheme] = json.load(args.infile)
+
+    dispatcher = Dispatcher(
+        handlers=[
+            GitHubHandler(args.github_user, args.github_token),
+            BugsOpenJdkHandler(),
+        ]
+    )
+    issues_with_status = fetch_all_statuses(issues, dispatcher, args.max_workers)
+
+    LOG.debug(f"Outputting JSON to {getattr(args.outfile, 'name', '<unknown>')}")
+    json.dump(
+        obj=issues_with_status,
+        fp=args.outfile,
+        indent=2,
+    )
 
 
 if __name__ == '__main__':
     main()
-
-
-
