eaa5dcaba8376bad25186af3fbbd67873395f67b
amir-waheed <amir2@ualberta.ca>
2022-03-21 05:28:09 -0600

    Changing type to secret text
    Signed-off-by: amir-waheed <amir2@ualberta.ca>

--- buildenv/jenkins/triggerGrinder.groovy
+++ buildenv/jenkins/triggerGrinder.groovy
@@ -47,9 +47,9 @@ def launch_grinders(List<Map<String, Object>> json) {
 def trigger_issue_status() {
 
   if (params.AQA_ISSUE_TRACKER_CREDENTIAL_ID) {
-    withCredentials([usernamePassword(credentialsId: "${params.AQA_ISSUE_TRACKER_CREDENTIAL_ID}", variable: 'PASSWORD')]) {
+    withCredentials([string(credentialsId: "${params.AQA_ISSUE_TRACKER_CREDENTIAL_ID}", variable: 'TOKEN')]) {
         sh """exoprt AQA_ISSUE_TRACKER_GITHUB_USER = eclipse_aqavit
-        export AQA_ISSUE_TRACKER_GITHUB_TOKEN = ${PASSWORD}
+        export AQA_ISSUE_TRACKER_GITHUB_TOKEN = ${TOKEN}
         python3 ${WORKSPACE}/aqa-tests/scripts/disabled_tests/issue_status.py --infile problem_list.json > ${WORKSPACE}/scripts/disabled_tests/output.json"""
     }
   }
