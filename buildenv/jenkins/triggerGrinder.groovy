0493c343435cb1c7fd0cdfde153c1149f18367a5
amir-waheed <amir2@ualberta.ca>
2022-03-21 05:30:25 -0600

    Ttypo fix
    Signed-off-by: amir-waheed <amir2@ualberta.ca>

--- buildenv/jenkins/triggerGrinder.groovy
+++ buildenv/jenkins/triggerGrinder.groovy
@@ -48,7 +48,7 @@ def trigger_issue_status() {
 
   if (params.AQA_ISSUE_TRACKER_CREDENTIAL_ID) {
     withCredentials([string(credentialsId: "${params.AQA_ISSUE_TRACKER_CREDENTIAL_ID}", variable: 'TOKEN')]) {
-        sh """exoprt AQA_ISSUE_TRACKER_GITHUB_USER = eclipse_aqavit
+        sh """export AQA_ISSUE_TRACKER_GITHUB_USER = eclipse_aqavit
         export AQA_ISSUE_TRACKER_GITHUB_TOKEN = ${TOKEN}
         python3 ${WORKSPACE}/aqa-tests/scripts/disabled_tests/issue_status.py --infile problem_list.json > ${WORKSPACE}/scripts/disabled_tests/output.json"""
     }
