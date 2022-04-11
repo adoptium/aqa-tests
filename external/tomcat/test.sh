0ab0bedc82969885fbcb49758232e5c9d5be7a4b
yinggzz <74112748+yinggzz@users.noreply.github.com>
2022-04-11 13:12:54 -0400

    update tomcat test.properties and test.sh (#3507)

--- external/tomcat/test.sh
+++ external/tomcat/test.sh
@@ -16,9 +16,31 @@ source $(dirname "$0")/test_base_functions.sh
 #Set up Java to be used by the tomcat test
 echo_setup
 
+git clone -q -b 1.6.x --single-branch https://github.com/apache/apr.git $TEST_HOME/tmp/apr
+cd $TEST_HOME/tmp/apr
+./buildconf
+./configure --prefix=$TEST_HOME/tmp/apr-build
+make
+make install
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$TEST_HOME/tmp/apr-build/lib"
+
+git clone -q https://github.com/apache/tomcat-native.git $TEST_HOME/tmp/tomcat-native
+cd $TEST_HOME/tmp/tomcat-native/native
+sh buildconf --with-apr=$TEST_HOME/tmp/apr
+./configure --with-apr=$TEST_HOME/tmp/apr --with-java-home=$JAVA_HOME --with-ssl=yes --prefix=$TEST_HOME/tmp/tomcat-native-build
+make
+make install
+cd $TEST_HOME
+yes | cp build.properties.default build.properties
+echo >> build.properties
+echo "test.threads=4" >> build.properties
+echo "test.relaxTiming=true" >> build.properties
+echo "test.excludePerformance=true" >> build.properties
+echo "test.openssl.path=/dev/null/openssl" >> build.properties
+echo "test.apr.loc=$TEST_HOME/tmp/tomcat-native-build/lib" >> build.properties
+
 set -e
 echo "Building tomcat" && \
-cp build.properties.default build.properties && \
 ant && \
 
 echo "Running tomcat tests" 
