d0da8545f03b57df07e107d7ead36111ff5a974e
Shelley Lambert <slambert@gmail.com>
2022-05-20 13:48:17 -0400

    Start from TEST_RESROOT
    Signed-off-by: Shelley Lambert <slambert@gmail.com>

--- openjdk/openjdk.mk
+++ openjdk/openjdk.mk
@@ -176,5 +176,5 @@ endif
 
 VENDOR_PROBLEM_LIST_FILE:=
 ifneq (,$(findstring Red Hat,$(JDK_VENDOR)))
-	VENDOR_PROBLEM_LIST_FILE:=-exclude:$(Q)excludes$(D)vendors$(D)redhat$(D)ProblemList_openjdk$(JDK_VERSION).txt$(Q)
+	VENDOR_PROBLEM_LIST_FILE:=-exclude:$(Q)$(TEST_RESROOT)$(D)excludes$(D)vendors$(D)redhat$(D)ProblemList_openjdk$(JDK_VERSION).txt$(Q)
 endif
