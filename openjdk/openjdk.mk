30b52a5d81b2525f7a29b78555df17e759eecc87
Sophia Guo <sophia.gwf@gmail.com>
2022-06-21 16:16:05 -0400

    Temp change for test only
    Signed-off-by: Sophia Guo <sophia.gwf@gmail.com>

--- openjdk/openjdk.mk
+++ openjdk/openjdk.mk
@@ -72,7 +72,8 @@ JTREG_BASIC_OPTIONS += -a
 JTREG_ASSERT_OPTION = -ea -esa
 JTREG_BASIC_OPTIONS += $(JTREG_ASSERT_OPTION)
 # Report details on all failed or error tests, times, and suppress output for tests that passed
-JTREG_BASIC_OPTIONS += -v:fail,error,time,nopass
+#####this change only for test, which should not be merged
+JTREG_BASIC_OPTIONS += -v:fail,error,time,pass
 # Retain all files for failing tests
 JTREG_BASIC_OPTIONS += -retain:fail,error,*.dmp,javacore.*,heapdump.*,*.trc
 # Ignore tests are not run and completely silent about it
