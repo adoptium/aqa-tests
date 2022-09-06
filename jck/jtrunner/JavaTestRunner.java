749ef50b10fcf28d5629cd6a25a54567be97d250
sobirada <58551844+sobirada@users.noreply.github.com>
2022-09-06 12:53:09 +0530

    Update JavaTestRunner.java

--- jck/jtrunner/JavaTestRunner.java
+++ jck/jtrunner/JavaTestRunner.java
@@ -643,10 +643,6 @@ public class JavaTestRunner {
 					fileContent += "set jck.env.runtime.remoteAgent.passiveHost localhost" + ";\n";
 					fileContent += "set jck.env.runtime.remoteAgent.passivePortDefault Yes" + ";\n";
 					
-					if (tests.contains("vm/jdwp")) {
-					fileContent += "set jck.env.runtime.jdwp.VMSuspended No" + ";\n";
-					fileContent += "set jck.env.runtime.jdwp.jdwpOpts -agentlib\:jdwp\=server\=y,transport\=dt_socket,address\=localhost\:35000,suspend\=n"  + ";\n";
-					}
 				}
 			}
 			// Without the following override the following failures occur:
