fa5ba2f37cc22c24d2bf04a4cbb260f27cd0ce87
Soumya Biradar <soumyabiradar@Soumyas-MacBook-Pro.local>
2022-09-06 14:59:14 +0530

    chaging escape character \ to \

--- jck/jtrunner/JavaTestRunner.java
+++ jck/jtrunner/JavaTestRunner.java
@@ -646,7 +646,7 @@ public class JavaTestRunner {
 					if((tests.contains("vm/jdwp")) && (platform.equals("aix"))) {
 					
 						fileContent += "set jck.env.runtime.jdwp.VMSuspended No" + ";\n";
-						fileContent += "set jck.env.runtime.jdwp.jdwpOpts -agentlib\:jdwp\=server\=y,transport\=dt_socket,address\=localhost\:35000,suspend\=n"  + ";\n";
+						fileContent += "set jck.env.runtime.jdwp.jdwpOpts -agentlib\\:jdwp\\=server\\=y,transport\\=dt_socket,address\\=localhost\\:35000,suspend\\=n"  + ";\n";
 						}
 				}
 			}
