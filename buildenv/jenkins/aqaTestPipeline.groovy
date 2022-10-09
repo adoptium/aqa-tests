517072d10b009227978aa7049798663a002377ae
!--Julian-- <78744671+julian55455@users.noreply.github.com>
2022-10-09 23:37:50 +0300

    Set default USE_TESTENV_PROPERTIES value to false (#4022)

--- buildenv/jenkins/aqaTestPipeline.groovy
+++ buildenv/jenkins/aqaTestPipeline.groovy
@@ -4,7 +4,7 @@ def JDK_VERSIONS = params.JDK_VERSIONS.trim().split("\\s*,\\s*");
 def PLATFORMS = params.PLATFORMS.trim().split("\\s*,\\s*");
 def TARGETS = params.TARGETS.trim().split("\\s*,\\s*");
 
-def USE_TESTENV_PROPERTIES = params.USE_TESTENV_PROPERTIES ? params.USE_TESTENV_PROPERTIES : true
+def USE_TESTENV_PROPERTIES = params.USE_TESTENV_PROPERTIES ? params.USE_TESTENV_PROPERTIES : false
 def PARALLEL = params.PARALLEL ? params.PARALLEL : "Dynamic"
 def NUM_MACHINES = params.NUM_MACHINES ? params.NUM_MACHINES : 3
 def SDK_RESOURCE = params.SDK_RESOURCE ? params.SDK_RESOURCE : "releases"
