f81b58024351d125c6ab61545ca58ac4b742fd74
Martijn Verburg <martijnverburg@gmail.com>
2023-03-20 21:49:30 +1300

    Add in guidance from test plan

--- perf/specjbb/run/options/specjbb-multijvm-simple.sh
+++ perf/specjbb/run/options/specjbb-multijvm-simple.sh
@@ -8,7 +8,7 @@ export GROUP_COUNT=1
 export TI_JVM_COUNT=1 
 export NUM_OF_RUNS=1
 
-export SPECJBB_OPTS_C="-Dspecjbb.group.count=$GROUP_COUNT -Dspecjbb.txi.pergroup.count=$TI_JVM_COUNT"
+export SPECJBB_OPTS_C="-Dspecjbb.group.count=$GROUP_COUNT -Dspecjbb.txi.pergroup.count=$TI_JVM_COUNT -Dspecjbb.forkjoin.workers.Tier1=128 -Dspecjbb.forkjoin.workers.Tier2=14 -Dspecjbb.forkjoin.workers.Tier3=24 -Dspecjbb.customerDriver.threads.saturate=96 -Dspecjbb.comm.connect.selector.runner.count=10"
 export SPECJBB_OPTS_TI=""
 export SPECJBB_OPTS_BE=""
 
@@ -18,8 +18,13 @@ export SPECJBB_OPTS_BE=""
 #
 # This implies that a machine should have atleast 8GB of memory and 8 CPU cores to run this test
 export JAVA_OPTS_C="-Xms2g -Xmx2g -Xmn1536m -XX:+UseParallelGC -XX:ParallelGCThreads=2 -XX:CICompilerCount=4"
-export JAVA_OPTS_TI="${JAVA_OPTS_C}" 
-export JAVA_OPTS_BE="-Xms4g -Xmx4g -Xmn3g -XX:+UseParallelGC -XX:ParallelGCThreads=4 -XX:-UseAdaptiveSizePolicy"
+export JAVA_OPTS_TI="${JAVA_OPTS_C}"
+
+# Default shakeout configuration
+# export JAVA_OPTS_BE="-Xms4g -Xmx4g -Xmn3g -XX:+UseParallelGC -XX:ParallelGCThreads=4 -XX:-UseAdaptiveSizePolicy"
+
+# Configuration for a Maxed out (from a scaling perspective) Standard_D64s_v5 run as per the Java Engineering Group Azure VM SKU SPECjbb2015 guide
+export JAVA_OPTS_BE="-Xms192g -Xmx192g -Xmn173g -XX:+UseParallelGC -XX:ParallelGCThreads=64 -XX:+AlwaysPreTouch -XX:+UseLargePages -XX:+UseTransparentHugePages -XX:-UseAdaptiveSizePolicy -XX:-UsePerfData"
 
 export MODE_ARGS_C=""
 export MODE_ARGS_TI=""
--- perf/specjbb/run/run_multi.sh
+++ perf/specjbb/run/run_multi.sh
@@ -7,6 +7,12 @@ echo "Results dir: ${RESULTS_DIR}"
 function runSpecJbbMulti() {
   for ((runNumber=1; runNumber<=NUM_OF_RUNS; runNumber=runNumber+1)); do
 
+    # Call sync to force any pending disk writes
+    sync
+
+    # Clear memory caches
+    echo 3 > /proc/sys/vm/drop_caches
+
     # Create temp result directory                
     local result
     result="${RESULTS_DIR%\"}"
@@ -30,7 +36,7 @@ function runSpecJbbMulti() {
     echo "Starting the Controller JVM"
     JAVA="${JAVA%\"}"
     JAVA="${JAVA#\"}"
-    # We don't double quote escape all arguments as some of those are being pssed in as a list with spaces
+    # We don't double quote escape all arguments as some of those are being passed in as a list with spaces
     # shellcheck disable=SC2086
     "${JAVA}" ${JAVA_OPTS_C} ${SPECJBB_OPTS_C} -jar "${SPECJBB_JAR}" -m MULTICONTROLLER ${MODE_ARGS_C} 2>controller.log 2>&1 | tee controller.out &
 
@@ -54,7 +60,7 @@ function runSpecJbbMulti() {
           local transactionInjectorName="$groupId.TxInjector.$transactionInjectorJvmId"
 
           echo "Start $transactionInjectorName"
-          # We don't double quote escape all arguments as some of those are being pssed in as a list with spaces
+          # We don't double quote escape all arguments as some of those are being passed in as a list with spaces
           # shellcheck disable=SC2086
           "${JAVA}" ${JAVA_OPTS_TI} ${SPECJBB_OPTS_TI} -jar "${SPECJBB_JAR}" -m TXINJECTOR -G=$groupId -J="${transactionInjectorJvmId}" ${MODE_ARGS_TI} > "${TI_NAME}.log" 2>&1 &
           echo -e "\t${transactionInjectorName} PID = $!"
@@ -66,10 +72,11 @@ function runSpecJbbMulti() {
       local backendJvmId=beJVM
       local backendName="$groupId.Backend.${backendJvmId}"
       
-      # Add GC logging to the backend's JVM options TODO update this to the real command
-      JAVA_OPTS_BE_WITH_GC_LOG="$JAVA_OPTS_BE -Xlog:gc*:file=${backendName}_gc.log"
+      # Add GC logging to the backend's JVM options. We use the recommendended settings for Microsoft's internal GC analysis tool called Censum
+      JAVA_OPTS_BE_WITH_GC_LOG="$JAVA_OPTS_BE -Xlog:gc*,gc+ref=debug,gc+phases=debug,gc+age=trace,safepoint:file=${backendName}_gc.log"
+
       echo " Start $BE_NAME"
-      # We don't double quote escape all arguments as some of those are being pssed in as a list with spaces
+      # We don't double quote escape all arguments as some of those are being passed in as a list with spaces
       # shellcheck disable=SC2086
       "${JAVA}" ${JAVA_OPTS_BE_WITH_GC_LOG} ${SPECJBB_OPTS_BE} -jar "${SPECJBB_JAR}" -m BACKEND -G=$groupId -J=$backendJvmId ${MODE_ARGS_BE} > "${backendName}.log" 2>&1 &
       echo -e "\t$BE_NAME PID = $!"
@@ -97,4 +104,5 @@ function runSpecJbbMulti() {
 
 runSpecJbbMulti
 
+# exit gracefully once we are done
 exit 0
