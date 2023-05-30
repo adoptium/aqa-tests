d4e891679b27e9248405eb40397c178f4e985c52
Mesbah Alam <Mesbah_Alam@ca.ibm.com>
2023-05-30 19:12:15 -0400

    Use JavaTestUtil for all tck (#4512)
    Signed-off-by: Mesbah Alam <Mesbah_Alam@ca.ibm.com>

--- jck/agent-drive.sh
+++ jck/agent-drive.sh
@@ -1,14 +1,84 @@
-#!/bin/bash
+#!/bin/bash -ue
 
-echo "Starting JCK agent.."
-eval $1
+jckAgentPID=0
+rmiRegistryPID=0
+rmidPID=0
+tnameservPID=0
 
-pid=$!
+startJCKAgent() {
+	echo "Starting JCK agent.."
+	eval $1
+	jckAgentPID=$!
+	echo "Agent started with PID $jckAgentPID"
+}
 
-echo "Agent started with PID $pid" 
+stopJCKAgent() {
+	echo "Test complete. Stopping JCK Agent.."
+	kill -9 $jckAgentPID
+}
 
-echo "Starting JCK harness.."
-eval $2
+startJCKHarness() {
+	echo "Starting JCK harness.."
+	eval $1
+}
 
-echo "Test complete. Stopping JCK Agent.." 
-kill -9 $pid
+startRMIRegistry() {
+	echo "Starting RMI Registry.."
+	eval $1
+	rmiRegistryPID=$!
+	echo "RMI Registry started with PID $rmiRegistryPID"
+} 
+
+stopRMIRegistry() {
+	echo "Stopping RMI Registry.."
+	kill -9 $rmiRegistryPID
+}
+
+startRMID() {
+	echo "Starting RMID.."
+	eval $1
+	rmidPID=$!	
+	echo "RMID started with PID $rmidPID"
+}
+
+stopRMID() {
+	echo "Stopping RMID.."
+	kill -9 $rmidPID
+}
+
+startTNameServ() {
+	echo "Starting TNAMESERV.."
+	eval $1
+	tnameservPID=$!	
+	echo "TNAMESERV started with PID $tnameservPID"
+}
+
+stopTNameServ() {
+	echo "Stopping TNAMESERV.."
+	kill -9 $tnameservPID
+}
+
+if [ $# -eq 2 ]; then
+	startJCKAgent "$1"
+	startJCKHarness "$2"
+	stopJCKAgent
+elif [ $# -eq 4 ]; then
+	startRMIRegistry "$1"
+	startRMID "$2"
+	startJCKAgent "$3"
+	startJCKHarness "$4"
+	stopRMIRegistry
+	stopRMID
+	stopJCKAgent
+elif [ $# -eq 5 ]; then
+	startRMIRegistry "$1"
+	startRMID "$2"
+	startTNameServ "$3"
+	startJCKAgent "$4"
+	startJCKHarness "$5"
+	stopRMIRegistry
+	stopRMID
+	stopJCKAgent
+	stopTNameServ
+	stopJCKAgent
+fi
