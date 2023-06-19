d2b6bb4c2c02458183a00c4143e2b9aa763ea028
Soumya Biradar <soumyabiradar@Soumyas-MacBook-Pro.local>
2023-06-19 19:24:34 +0530

    adding workaround for time interrupts

--- jck/jtrunner/JavatestUtil.java
+++ jck/jtrunner/JavatestUtil.java
@@ -610,7 +610,7 @@ public class JavatestUtil {
 					fileContent += "set jck.env.runtime.remoteAgent.passiveHost localhost" + ";\n";
 					fileContent += "set jck.env.runtime.remoteAgent.passivePortDefault Yes" + ";\n";
 
-				if (test.contains("vm/jdwp")) {
+				if (tests.contains("vm/jdwp")) {
 					fileContent += "set jck.env.runtime.jdwp.VMSuspended No" + ";\n";
 					fileContent += "set jck.env.runtime.jdwp.jdwpOpts -agentlib\\:jdwp\\=server\\=y,transport\\=dt_socket,address\\=localhost\\:35000,suspend\\=n" + ";\n";
 		
