7b6a52e082b3443678d4a1f4fc0e5980b4fc04ab
Soumya Biradar <soumyabiradar@Soumyas-MacBook-Pro.local>
2023-06-19 19:18:54 +0530

    adding workaround for time interrupts

--- jck/jtrunner/JavaTestRunner.java
+++ jck/jtrunner/JavaTestRunner.java
@@ -671,17 +671,15 @@ public class JavaTestRunner {
 						!tests.contains("api/org_omg/PortableServer") ) {
 					fileContent += "set jck.env.runtime.remoteAgent.passiveHost localhost" + ";\n";
 					fileContent += "set jck.env.runtime.remoteAgent.passivePortDefault Yes" + ";\n";
-					fileContent += "set jck.env.runtime.jdwp.VMSuspended No" + ";\n";
-					fileContent += "set jck.env.runtime.jdwp.jdwpOpts -agentlib\\:jdwp\\=server\\=y,transport\\=dt_socket,address\\=localhost\\:35000,suspend\\=n" + ";\n";
-						}
+					}
 
 					}
 						
 			// if ( tests.contains("vm/jdwp") )
 			
 					// {
-						fileContent += "set jck.env.runtime.jdwp.VMSuspended No" + ";\n";
-						fileContent += "set jck.env.runtime.jdwp.jdwpOpts -agentlib\\:jdwp\\=server\\=y,transport\\=dt_socket,address\\=localhost\\:35000,suspend\\=n" + ";\n";
+						// fileContent += "set jck.env.runtime.jdwp.VMSuspended No" + ";\n";
+						// fileContent += "set jck.env.runtime.jdwp.jdwpOpts -agentlib\\:jdwp\\=server\\=y,transport\\=dt_socket,address\\=localhost\\:35000,suspend\\=n" + ";\n";
 					// } 
 
 							
--- jck/jtrunner/JavatestUtil.java
+++ jck/jtrunner/JavatestUtil.java
@@ -609,6 +609,14 @@ public class JavatestUtil {
 						!tests.contains("api/org_omg/PortableServer") ) {
 					fileContent += "set jck.env.runtime.remoteAgent.passiveHost localhost" + ";\n";
 					fileContent += "set jck.env.runtime.remoteAgent.passivePortDefault Yes" + ";\n";
+
+				if (test.contains("vm/jdwp")) {
+					fileContent += "set jck.env.runtime.jdwp.VMSuspended No" + ";\n";
+					fileContent += "set jck.env.runtime.jdwp.jdwpOpts -agentlib\\:jdwp\\=server\\=y,transport\\=dt_socket,address\\=localhost\\:35000,suspend\\=n" + ";\n";
+		
+				}
+
+
 				}
 			}
 			
