7dc97e97d49d3aa3c285b8e990d1443efebf92b6
Soumya Biradar <soumyabiradar@Soumyas-MacBook-Pro.local>
2023-06-22 18:49:49 +0530

    changing \ to \* in JavatestUtil.java

--- jck/jtrunner/JavatestUtil.java
+++ jck/jtrunner/JavatestUtil.java
@@ -612,8 +612,8 @@ public class JavatestUtil {
 
 				if (tests.contains("vm/jdwp")) {
 					fileContent += "set jck.env.runtime.jdwp.VMSuspended No" + ";\n";
-					fileContent += "set jck.env.runtime.jdwp.jdwpOpts -agentlib\\:jdwp\\=server\\=y,transport\\=dt_socket,address\\=localhost\\:35000,suspend\\=n" + ";\n";
-		
+					fileContent += "set jck.env.runtime.jdwp.jdwpOpts -agentlib\":jdwp\"=server\"=y,transport\"=dt_socket,address\"=localhost\":35000,suspend\"=n" + ";\n";
+
 				}
 
 
