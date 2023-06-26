3391b3f224907e5aa62004c57dfc70282c40e34f
Lan Xia <Lan_Xia@ca.ibm.com>
2023-06-26 17:45:04 -0400

    Support CRIU portable testing on plinux (#4646)
    Signed-off-by: Lan Xia <Lan_Xia@ca.ibm.com>

--- external/criuSettings.mk
+++ external/criuSettings.mk
@@ -24,5 +24,5 @@ export CRIU_COMBO_LIST_linux_390_64_z15=sw.os.ubuntu.22-hw.arch.s390x.z15 sw.os.
 export CRIU_COMBO_LIST_aarch64_linux=sw.os.ubuntu.22-hw.arch.aarch64.armv8 sw.os.rhel.9-hw.arch.aarch64.armv8
 # not available: sw.os.rhel.8-hw.arch.aarch64.armv8 sw.os.ubuntu.20-hw.arch.aarch64.armv8
 
-# placeholder 
-export CRIU_COMBO_LIST_ppc64le_linux=
+export CRIU_COMBO_LIST_ppc64le_linux=sw.os.rhel.8-hw.arch.ppc64le.p8 sw.os.rhel.8-hw.arch.ppc64le.p10 sw.os.rhel.9-hw.arch.ppc64le.p9
+# not available: sw.os.rhel.8-hw.arch.ppc64le.p9 sw.os.rhel.9-hw.arch.ppc64le.p8 sw.os.rhel.9-hw.arch.ppc64le.p10
