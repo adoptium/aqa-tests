8388e345043720011e0520b4f8eefb1613948dd4
LongyuZhang <longyu.zhang@ibm.com>
2024-02-29 17:06:07 -0500

    Temporarily disable criu test on rhel9 skylake (#5115)
    - Since rhel 9 skylake machine is missing, temporarily disable it from pipeline
    
    Signed-off-by: LongyuZhang <longyu.zhang@ibm.com>

--- external/criuSettings.mk
+++ external/criuSettings.mk
@@ -13,8 +13,8 @@
 ##############################################################################
 
 # - is shell metacharacter. In PLATFORM value, replace - with _
-export CRIU_COMBO_LIST_linux_x86_64=sw.os.ubuntu.22-hw.arch.x86.broadwell sw.os.ubuntu.22-hw.arch.x86.amd sw.os.rhel.8-hw.arch.x86.broadwell sw.os.rhel.8-hw.arch.x86.amd sw.os.rhel.8-hw.arch.x86.skylake sw.os.rhel.9-hw.arch.x86.amd sw.os.rhel.9-hw.arch.x86.skylake
-# not available: sw.os.ubuntu.22-hw.arch.x86.skylake sw.os.rhel.9-hw.arch.x86.broadwell
+export CRIU_COMBO_LIST_linux_x86_64=sw.os.ubuntu.22-hw.arch.x86.broadwell sw.os.ubuntu.22-hw.arch.x86.amd sw.os.rhel.8-hw.arch.x86.broadwell sw.os.rhel.8-hw.arch.x86.amd sw.os.rhel.8-hw.arch.x86.skylake sw.os.rhel.9-hw.arch.x86.amd sw.os.rhel.9-hw.arch.x86.broadwell
+# not available: sw.os.ubuntu.22-hw.arch.x86.skylake sw.os.rhel.9-hw.arch.skylake
 
 export CRIU_COMBO_LIST_linux_390_64_z13=sw.os.ubuntu.22-hw.arch.s390x.z13 sw.os.ubuntu.22-hw.arch.s390x.z15 sw.os.rhel.8-hw.arch.s390x.z15 sw.os.rhel.9-hw.arch.s390x.z15
 # not available: sw.os.rhel.8-hw.arch.s390x.z13
