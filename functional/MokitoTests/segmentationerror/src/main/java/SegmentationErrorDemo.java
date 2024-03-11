04fbc77e7e07300874984555ad905e7e2550c38d
sophiaxu0424 <xuminghong0424@gmail.com>
2024-03-11 12:12:14 -0400

    Added mokitotest folder sample
    Signed-off-by: sophiaxu0424 <xuminghong0424@gmail.com>

---
+++ functional/MokitoTests/segmentationerror/src/main/java/SegmentationErrorDemo.java
@@ -0,0 +1,10 @@
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+
+public class SegmentationErrorDemo {
+    public static void main(String[] args) {
+        Mockito.mock(ArrayList.class);
+    }
+
+}
