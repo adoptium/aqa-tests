794cdb2228cf39b7b5419222b08b9c224f8731ff
sophiaxu0424 <xuminghong0424@gmail.com>
2024-04-09 13:50:51 -0400

    fix all
    Signed-off-by: sophiaxu0424 <xuminghong0424@gmail.com>

--- functional/MockitoTests/src/test/java/SegmentationErrorDemo.java
+++ functional/MockitoTests/src/test/java/SegmentationErrorDemo.java
@@ -3,7 +3,7 @@ package test.java;
 import java.util.ArrayList;
 import org.mockito.Mockito;
 
-public class Main {
+public class SegmentationErrorDemo {
     public static void main(String[] args) {
         Mockito.mock(ArrayList.class);
     }
