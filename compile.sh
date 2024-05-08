8f4eea56846afaac72a84f0d5f1ed7f5f3b19116
Lan Xia <Lan_Xia@ca.ibm.com>
2024-05-08 14:58:43 -0400

    Support OPENJCEPLUS_GIT_REPO and OPENJCEPLUS_GIT_BRANCH in testenv.properties (#5291)
    - create testenvSettings.sh for testenv variable settings
    - export OPENJCEPLUS_GIT_REPO and OPENJCEPLUS_GIT_BRANCH values in testenvSettings.sh
    - update functional/OpenJcePlusTests/build.xml to handle branch/tag/sha values
    - add JDKXXX_OPENJCEPLUS_GIT_REPO and JDKXXX_OPENJCEPLUS_GIT_BRANCH in testenv.properties
    - when DYNAMIC_COMPILE=true, we will run make test command directly (compile.sh will be skipped)
    To fix https://github.com/adoptium/aqa-tests/issues/4509, testenvSettings.sh needs to run before make test command
    
    resolves: https://github.com/adoptium/aqa-tests/issues/5290 and https://github.com/adoptium/aqa-tests/issues/4509
    
    Signed-off-by: Lan Xia <Lan_Xia@ca.ibm.com>

--- compile.sh
+++ compile.sh
@@ -8,36 +8,8 @@ if [ $(uname) = AIX ] || [ $(uname) = SunOS ] || [ $(uname) = *BSD ]; then
 else
     MAKE=make
 fi
-if [ $USE_TESTENV_PROPERTIES == true ]; then
-    testenv_file="./testenv/testenv.properties"
-    if [[ "$PLATFORM" == *"zos"* ]]; then
-        testenv_file="./testenv/testenv_zos.properties"
-    fi
-    if [[ "$PLATFORM" == *"arm"* ]] && [[ "$JDK_VERSION" == "8" ]]; then
-        testenv_file="./testenv/testenv_arm32.properties"
-    fi
-    while read line; do
-        export $line
-    done <$testenv_file
-    if [ $JDK_IMPL == "openj9" ] || [ $JDK_IMPL == "ibm" ]; then
-        repo=JDK${JDK_VERSION}_OPENJ9_REPO
-        branch=JDK${JDK_VERSION}_OPENJ9_BRANCH
-    else
-        repo=JDK${JDK_VERSION}_REPO
-        branch=JDK${JDK_VERSION}_BRANCH
-    fi
 
-    eval repo2='$'$repo
-    eval branch2='$'$branch
+source ./scripts/testenv/testenvSettings.sh
 
-    export JDK_REPO=$repo2
-    export JDK_BRANCH=$branch2
-    echo "Set values based on ${testenv_file}:"
-    cat $testenv_file
-    echo ""
-    echo "JDK_REPO=${JDK_REPO}"
-    echo "JDK_BRANCH=${JDK_BRANCH}"
-
-fi
 cd ./TKG
 $MAKE compile
---
+++ scripts/testenv/testenvSettings.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+set +x
+set -eo pipefail
+
+if [[ $USE_TESTENV_PROPERTIES == true ]]; then
+    testenv_file="./testenv/testenv.properties"
+    if [[ "$PLATFORM" == *"zos"* ]]; then
+        testenv_file="./testenv/testenv_zos.properties"
+    fi
+    if [[ "$PLATFORM" == *"arm"* ]] && [[ "$JDK_VERSION" == "8" ]]; then
+        testenv_file="./testenv/testenv_arm32.properties"
+    fi
+    while read line; do
+        export $line
+    done <$testenv_file
+    if [[ $JDK_IMPL == "openj9" ]] || [[ $JDK_IMPL == "ibm" ]]; then
+        repo=JDK${JDK_VERSION}_OPENJ9_REPO
+        branch=JDK${JDK_VERSION}_OPENJ9_BRANCH
+
+        openjceplus_repo=JDK${JDK_VERSION}_OPENJCEPLUS_GIT_REPO
+        openjceplus_branch=JDK${JDK_VERSION}_OPENJCEPLUS_GIT_BRANCH
+        eval openjceplus_repo2='$'$openjceplus_repo
+        eval openjceplus_branch2='$'$openjceplus_branch
+        export OPENJCEPLUS_GIT_REPO=$openjceplus_repo2
+        export OPENJCEPLUS_GIT_BRANCH=$openjceplus_branch2
+
+    else
+        repo=JDK${JDK_VERSION}_REPO
+        branch=JDK${JDK_VERSION}_BRANCH
+    fi
+
+    eval repo2='$'$repo
+    eval branch2='$'$branch
+
+    export JDK_REPO=$repo2
+    export JDK_BRANCH=$branch2
+    echo "Set values based on ${testenv_file}:"
+    echo "========="
+    cat $testenv_file
+    echo ""
+    echo "========="
+    echo ""
+    echo "JDK_REPO=${JDK_REPO}"
+    echo "JDK_BRANCH=${JDK_BRANCH}"
+    echo "OPENJCEPLUS_GIT_REPO=${OPENJCEPLUS_GIT_REPO}"
+    echo "OPENJCEPLUS_GIT_BRANCH=${OPENJCEPLUS_GIT_BRANCH}"
+
+fi
