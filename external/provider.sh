b185c9ae08ff25dbdc0fcd9b142a33c73250b87a
judovana <judovana@email.cz>
2024-07-31 09:29:52 +0200

    determining/determines
    Co-authored-by: Martijn Verburg <martijnverburg@gmail.com>

--- external/provider.sh
+++ external/provider.sh
@@ -16,7 +16,7 @@
 
 ###################################################################
 ## This script is walking through all external tests             ##
-## and is determining what virtualisation to use                 ##
+## and determines which virtualisation to use                    ##
 ## It recognizes two environment variables:                      ##
 ## EXTERNAL_AQA_RUNNER=docker/podman/...                         ##
 ## EXTERNAL_AQA_SUDO=sudo//runas                                 ##
