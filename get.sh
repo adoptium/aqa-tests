7aa94f86186778395d1e06232f6565dbc5fa51a5
LongyuZhang <longyu.zhang@ibm.com>
2025-02-18 12:03:01 -0500

    Skip download install zip (#5949) (#5960)
    Signed-off-by: Lan Xia <Lan_Xia@ca.ibm.com>
    Co-authored-by: Lan Xia <Lan_Xia@ca.ibm.com>

--- get.sh
+++ get.sh
@@ -295,7 +295,7 @@ getBinaryOpenjdk()
 						if [ "$TEST_IMAGES_REQUIRED" == "true" ]; then
 							download_url+=" ${download_url_base}${n}"
 						fi
-					else
+					elif [[ $n != *"install"* ]]; then
 						download_url+=" ${download_url_base}${n}"
 					fi
 				done
