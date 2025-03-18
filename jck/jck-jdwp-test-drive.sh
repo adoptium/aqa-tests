7781d2def7fbd1beaeffad3fc156c8d8a7b923cf
Haroon Khel <hkhel@redhat.com>
2025-03-18 13:10:53 +0000

    Timeout, pass generated jtb to script

--- jck/jck-jdwp-test-drive.sh
+++ jck/jck-jdwp-test-drive.sh
@@ -2,7 +2,7 @@
 
 jckAgentPID=0
 harnessExitCode=0
-tnameservPID=0
+jckHarnessPID=0
 
 startJCKAgent() {
 	echo "Starting JCK agent.."
@@ -16,34 +16,35 @@ stopJCKAgent() {
 	kill -9 $jckAgentPID
 }
 
-startTNameServ() {
-	echo "Starting TNAMESERV.."
-	eval $1
-	tnameservPID=$!	
-	echo "TNAMESERV started with PID $tnameservPID"
-}
-
 startJCKHarness() {
 	echo "Starting JCK harness.."
-	eval $1; return $?
+	eval $1
+    jckHarnessPID=$!
 }
 
-stopTNameServ() {
-	echo "Stopping TNAMESERV.."
-	kill -9 $tnameservPID
+queryVmJdwpTest() {
+    java -cp /home/jenkins/jck_root/JCK8-unzipped/JCK-runtime-8d/lib/javatest.jar com.sun.javatest.finder.ShowTests -finder com.sun.javatest.finder.HTMLTestFinder -end /home/jenkins/jck_root/JCK8-unzipped/JCK-runtime-8d/tests/testsuite.html -initial vm/jdwp | tr -d "[:blank:]"
 }
+
 
 # Query all of the tests in $(JCK_ROOT)/JCK-runtime-$(JCK_VERSION_NUMBER)/tests/vm/jdwp/
-find $4 -type f -name "*.java" | while read -r test; 
+queryVmJdwpTest | while read -r test; 
 do
-    startJCKAgent
-    # cp generated.jtb temp_jdwp.jtb 
-    # Replace tests=vm/jdwp line with tests=<TEST_CASE>
-    sed -i -e 's/tests vm/jdwp/tests $test/g' 
-    # Not sure what to do here ^^
-    startTNameServ "$2"
-	startJCKHarness "$3"
-    harnessExitCode=$?
-    stopTNameServ
+    cp $3 temp_jdwp.jtb
+    # Replace tests=vm/jdwp line with tests=<Individual test case>
+    sed -i -e "s/tests vm/jdwp/tests $test/g" temp_jdwp.jtb
+    startJCKAgent "$1"
+	startJCKHarness "$2" temp_jdwp.jtb
+    sleep 60 # 60 second timeout
+    if kill -s 0 $jckHarnessPID 2>nul; then
+        echo "Testcase $test : Process $jckHarnessPID is still running after 60 seconds... killing..."
+        kill -9 $jckHarnessPID
+        harnessExitCode=124
+    else
+        wait $jckHarnessPID
+        harnessExitCode=$?
+    fi
     stopJCKAgent
 done
+
+exit $harnessExitCode
--- jck/jck.mk
+++ jck/jck.mk
@@ -171,10 +171,11 @@ START_RMIREG = $(TEST_JDK_HOME)/bin/rmiregistry > $(REPORTDIR)$(D)rmiregistry.lo
 START_RMID = $(TEST_JDK_HOME)/bin/rmid -J-Dsun.rmi.activation.execPolicy=none -J-Djava.security.policy=$(JCK_ROOT)/JCK-runtime-$(JCK_VERSION_NUMBER)/lib/jck.policy > $(REPORTDIR)$(D)rmid.log &
 START_TNAMESRV = $(TEST_JDK_HOME)/bin/tnameserv -ORBInitialPort 9876 > $(REPORTDIR)$(D)tnameserv.log &
 EXEC_RUNTIME_TEST = $(JAVA_TO_TEST) -jar $(JCK_ROOT)/JCK-runtime-$(JCK_VERSION_NUMBER)/lib/javatest.jar -config $(CONFIG_ALT_PATH)/$(JCK_VERSION)/runtime.jti @$(REPORTDIR)/generated.jtb
+EXEC_RUNTIME_TEST_NO_JTB = $(JAVA_TO_TEST) -jar $(JCK_ROOT)/JCK-runtime-$(JCK_VERSION_NUMBER)/lib/javatest.jar -config $(CONFIG_ALT_PATH)/$(JCK_VERSION)/runtime.jti
 EXEC_COMPILER_TEST = $(JAVA_TO_TEST) -jar $(JCK_ROOT)/JCK-compiler-$(JCK_VERSION_NUMBER)/lib/javatest.jar -config $(CONFIG_ALT_PATH)/$(JCK_VERSION)/compiler.jti @$(REPORTDIR)/generated.jtb
 EXEC_DEVTOOLS_TEST = $(JAVA_TO_TEST) -jar $(JCK_ROOT)/JCK-devtools-$(JCK_VERSION_NUMBER)/lib/javatest.jar -config $(CONFIG_ALT_PATH)/$(JCK_VERSION)/devtools.jti @$(REPORTDIR)/generated.jtb
 EXEC_RUNTIME_TEST_WITH_AGENT = $(TEST_ROOT)/jck/agent-drive.sh '$(START_AGENT_GENERIC)' '$(EXEC_RUNTIME_TEST)'
-EXEC_RUNTIME_TEST_JDWP = $(TEST_ROOT)/jck/jck-jdwp-test-drive.sh '$(START_AGENT_GENERIC)' '$(EXEC_RUNTIME_TEST)' '$(JCK_ROOT)/JCK-runtime-$(JCK_VERSION_NUMBER)/tests/vm/jdwp/'
+EXEC_RUNTIME_TEST_JDWP = $(TEST_ROOT)/jck/jck-jdwp-test-drive.sh '$(START_AGENT_GENERIC)' '$(EXEC_RUNTIME_TEST_NO_JTB)' @$(REPORTDIR)/generated.jtb
 EXEC_RUNTIME_TEST_WITH_RMI_SERVICES = $(EXEC_RUNTIME_TEST_WITH_AGENT)
 
 ifeq ($(JDK_VERSION), 8)
