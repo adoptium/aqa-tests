75a349b639bc6f504815ed60d3b51960b3127b00
Shelley Lambert <slambert@gmail.com>
2025-06-02 00:37:22 -0400

    Strip down to only file name
    Signed-off-by: Shelley Lambert <slambert@gmail.com>

--- jck/interactives/arctic_tests/runTests.sh
+++ jck/interactives/arctic_tests/runTests.sh
@@ -69,7 +69,8 @@ do
 echo "testcase is $testcase"
    if [ -d $testcase ]; then
       echo "Starting testcase... $testcase"
-      tcase=$(basename $testcase .html)
+      tcase=${testcase##*/}
+      tcase=${s%.html}
 
       $TEST_JDK_HOME/bin/java --enable-preview --add-modules java.xml.crypto,java.sql -Djava.net.preferIPv4Stack=true -Djdk.attach.allowAttachSelf=true -Dsun.rmi.activation.execPolicy=none -Djdk.xml.maxXMLNameLimit=4000 -classpath :/home/jenkins/jck_root/JCK$VERSION-unzipped/JCK-runtime-$VERSION/classes: -Djava.security.policy=/home/jenkins/jck_root/JCK$VERSION-unzipped/JCK-runtime-$VERSION/lib/jck.policy javasoft.sqe.tests.api.java.awt.interactive.$tcase -TestCaseID ALL &
       
