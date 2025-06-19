e158476e4faad780219370fedaf8583435942856
Lan Xia <Lan_Xia@ca.ibm.com>
2025-06-19 13:27:35 -0400

    Streamline Perf Pipeline
    related: automation/issues/511
    Signed-off-by: Lan Xia <Lan_Xia@ca.ibm.com>

--- buildenv/jenkins/perfPipeline.groovy
+++ buildenv/jenkins/perfPipeline.groovy
@@ -36,8 +36,28 @@ params.each { param ->
 def perfConfigJson = []
 node("ci.role.test&&hw.arch.x86&&sw.os.linux") {
     checkout scm
+
     dir (env.WORKSPACE) {
-        def filePath = "./aqa-tests/perf/config/${params.JDK_IMPL}/"
+        def subdir = params.JDK_IMPL ?: "hotspot"
+        if (params.JDK_IMPL == "ibm") {
+            subdir = "openj9"
+        }
+        def filePath = "./aqa-tests/perf/config/${subdir}/"
+        // If vendor repo and branch is set, use vendor repo perfConfigJson file.
+        if (params.VENDOR_TEST_REPOS && params.VENDOR_TEST_BRANCHES) {
+            def vendorRepoDir = "vendorRepo"
+            def statusCode = -1
+            sshagent (credentials: ["$params.USER_CREDENTIALS_ID"], ignoreMissing: true) {
+                statusCode =  sh returnStatus: true, script: """
+                    git clone -q --depth 1 -b ${params.VENDOR_TEST_BRANCHES} ${params.VENDOR_TEST_REPOS} ${vendorRepoDir}
+                """
+            }
+            if (statusCode == 0) {
+                filePath = "./${vendorRepoDir}/perf/config/${subdir}/"
+            } else {
+                assert false: "Cannot git clone -b ${params.VENDOR_TEST_BRANCHES} ${params.VENDOR_TEST_REPOS}. Status code: ${statusCode}"
+            }
+        }
         filePath = filePath + "perfConfig.json"
         echo "Read JSON from file ${filePath}..."
         perfConfigJson = readJSON(file: "${filePath}")
