87b94e53e78985cb705a0781800886bdae22bbf9
Shelley Lambert <slambert@gmail.com>
2025-06-23 06:56:00 -0600

    Debugging
    Signed-off-by: Shelley Lambert <slambert@gmail.com>

--- jck/interactives/arctic_tests/runTests.sh
+++ jck/interactives/arctic_tests/runTests.sh
@@ -16,18 +16,76 @@
 wget -q https://ci.adoptium.net/job/Build_Arctic/5/artifact/upload/arctic-0.8.1.jar
 mv arctic-0.8.1.jar ${LIB_DIR}/arctic.jar
 
-JENKINS_HOME=/home/jenkins
-PPROP_LINE='s#arctic.common.repository.json.path.*$#arctic.common.repository.json.path = /home/jenkins/jck_run/arctic/mac/arctic_tests#g'
+setupLinuxEnv() {
+    # Set environment variables, makes the assumption that JDK21 is the default java in /usr/bin/java
+    export LC_ALL=POSIX
+    export ARCTIC_JDK=/usr/bin/java
+
+    echo "Fonts:"
+	echo "==========================================="
+	find /usr/share/fonts -name "*" -type d
+	echo "==========================================="
+
+	cp /etc/X11/twm/system.twmrc $HOME/.twmrc
+	echo 'RightTitleButton "xlogo11" = f.delete' >> $HOME/.twmrc
+	echo 'Button3 = : root : f.menu "windowops"' >> $HOME/.twmrc
+	echo 'RandomPlacement'                       >> $HOME/.twmrc
+
+	# Ensure fonts match recording vs playback
+	sed -i 's/MenuFont.*$/MenuFont    "-misc-fixed-bold-r-normal--15-140-75-75-c-90-iso8859-1"/g' $HOME/.twmrc
+	sed -i 's/TitleFont .*$/TitleFont   "-misc-fixed-bold-r-normal--15-140-75-75-c-90-iso8859-1"/g' $HOME/.twmrc
+	sed -i 's/IconFont .*$/IconFont    "-misc-fixed-bold-r-normal--15-140-75-75-c-90-iso8859-1"/g' $HOME/.twmrc
+	sed -i 's/ResizeFont .*$/ResizeFont    "-misc-fixed-bold-r-normal--15-140-75-75-c-90-iso8859-1"/g' $HOME/.twmrc
+	sed -i 's/IconManagerFont .*$/IconManagerFont    "-misc-fixed-bold-r-normal--15-140-75-75-c-90-iso8859-1"/g' $HOME/.twmrc
+
+	# Ensure consistent colors
+	sed -i 's/BorderColor.*$/BorderColor "slategrey"/g' $HOME/.twmrc
+	sed -i 's/DefaultBackground.*$/DefaultBackground "rgb:2\\/a\\/9"/g' $HOME/.twmrc
+	sed -i 's/DefaultForeground.*$/DefaultForeground "gray85"/g' $HOME/.twmrc
+	sed -i 's/TitleBackground.*$/TitleBackground "rgb:2\\/a\\/9"/g' $HOME/.twmrc
+	sed -i 's/TitleForeground.*$/TitleForeground "gray85"/g' $HOME/.twmrc
+	sed -i 's/MenuBackground.*$/MenuBackground "rgb:2\\/a\\/9"/g' $HOME/.twmrc
+	sed -i 's/MenuForeground.*$/MenuForeground "gray85"/g' $HOME/.twmrc
+	sed -i 's/MenuBorderColor.*$/MenuBorderColor "slategrey"/g' $HOME/.twmrc
+	sed -i 's/MenuTitleBackground.*$/MenuTitleBackground "gray70"/g' $HOME/.twmrc
+	sed -i 's/MenuTitleForeground.*$/MenuTitleForeground "rgb:2\\/a\\/9"/g' $HOME/.twmrc
+	sed -i 's/IconBackground.*$/IconBackground "rgb:2\\/a\\/9"/g' $HOME/.twmrc
+	sed -i 's/IconForeground.*$/IconForeground "gray85"/g' $HOME/.twmrc
+	sed -i 's/IconBorderColor.*$/IconBorderColor "gray85"/g' $HOME/.twmrc
+	sed -i 's/IconManagerBackground.*$/IconManagerBackground "rgb:2\\/a\\/9"/g' $HOME/.twmrc
+	sed -i 's/IconManagerForeground.*$/IconManagerForeground "gray85"/g' $HOME/.twmrc
+
+	# Start twm
+	twm &
+	twm_pid=$!
+	echo "Started twm process $twm_pid"
+}
+
+setupMacEnv() {
+
+}
+
+setupWindowsEnv() {
+
+}
+
 JOPTIONS="-Djava.net.preferIPv4Stack=true -Djdk.attach.allowAttachSelf=true -Dsun.rmi.activation.execPolicy=none -Djdk.xml.maxXMLNameLimit=4000"
-if [ $(uname) = SunOS ]; then
-    JENKINS_HOME = "/export/home/jenkins"
-    PPROP_LINE='s#arctic.common.repository.json.path.*$#arctic.common.repository.json.path = /export/home/jenkins/jck_run/arctic/mac/arctic_tests#g'
+
+if [ $(uname) = Linux ]; then
+    JENKINS_HOME=/home/jenkins
+    PPROP_LINE='s#arctic.common.repository.json.path.*$#arctic.common.repository.json.path = /home/jenkins/jck_run/arctic/mac/arctic_tests#g'
+    setupLinuxEnv
+
 elif [ $(uname) = Darwin ]; then
     JENKINS_HOME = "/Users/jenkins"
-    PPROP_LINE='s#arctic.common.repository.json.path.*$#arctic.common.repository.json.path = /Users/jenkins/jck_run/arctic/mac/arctic_tests#g'
+    PPROP_LINE='s#arctic.common.repository.json.path.*\$#arctic.common.repository.json.path = /Users/jenkins/jck_run/arctic/mac/arctic_tests#g'
+    setupMacEnv
+
 elif [ $(uname) = Windows_NT ]; then
     JENKINS_HOME = "c:/Users/jenkins"
     PPROP_LINE='s#arctic.common.repository.json.path.*$#arctic.common.repository.json.path = c:/Users/jenkins/jck_run/arctic/mac/arctic_tests#g'
+    setupWindowsEnv
+
 fi
 
 # Verify that the contents are present in jck_run
@@ -47,21 +105,12 @@ echo "TEST_DIR is $TEST_DIR"
 ls -al "$TEST_DIR"
 echo "TEST_GROUP is $TEST_GROUP, OSNAME is $OSNAME, VERSION is $VERSION", STARTING_SCOPE is $STARTING_SCOPE, TEST_SUB_DIR is $TEST_SUB_DIR
 
-# Set environment variables, makes the assumption that JDK21 is the default java in /usr/bin/java
-export LC_ALL=POSIX
-export ARCTIC_JDK=/usr/bin/java
+echo "Running testcases in $TEST_GROUP on $OSNAME"
+if [ $OSNAME = osx ]; then
+    $OSNAME = "mac"
+fi
 
-cp /etc/X11/twm/system.twmrc $HOME/.twmrc
-echo 'RightTitleButton "xlogo11" = f.delete' >> $HOME/.twmrc
-echo 'Button3 = : root : f.menu "windowops"' >> $HOME/.twmrc
-echo 'RandomPlacement'                       >> $HOME/.twmrc
-# Ensure fonts match recording vs playback
-sed -i 's/MenuFont.*$/MenuFont      "-misc-fixed-bold-r-normal--15-140-75-75-c-90-iso8859-1"/g' $HOME/.twmrc
-sed -i 's/TitleFont .*$/TitleFont   "-misc-fixed-bold-r-normal--15-140-75-75-c-90-iso8859-1"/g' $HOME/.twmrc
-sed -i 's/IconFont .*$/IconFont     "-misc-fixed-bold-r-normal--15-140-75-75-c-90-iso8859-1"/g' $HOME/.twmrc
-sed -i 's/ResizeFont .*$/ResizeFont "-misc-fixed-bold-r-normal--15-140-75-75-c-90-iso8859-1"/g' $HOME/.twmrc
-
-cp $JENKINS_HOME/jck_run/arctic/mac/player.properties .
+cp $JENKINS_HOME/jck_run/arctic/$OSNAME/player.properties .
 echo "Player properties line is $PPROP_LINE"
 sed -i "$PPROP_LINE" player.properties
 
@@ -71,17 +120,13 @@ if [ ! -f ${LIB_DIR}/arctic.jar ]; then
 fi
 
 echo "Starting player in background with RMI..."
-$ARCTIC_JDK -Darctic.logLevel=TRACE -jar ${LIB_DIR}/arctic.jar -p &
+# $ARCTIC_JDK -Darctic.logLevel=TRACE -jar ${LIB_DIR}/arctic.jar -p &
 rc=$?
 if [ $rc -ne 0 ]; then
    echo "Unable to start Arctic player, rc=$rc"
    exit $rc
 fi
 
-# Allow time for RMI server to start...
-sleep 5
-
-echo "Running testcases in $TEST_GROUP on $OSNAME"
 echo "Java under test: $TEST_JDK_HOME"
 twm &
 
