9c86daeb6a7b081e1fb6eebd9efe70ba3a57484c
Matt Wei <108814629+MattyWeee123@users.noreply.github.com>
2025-07-12 20:12:55 -0400

    Add GENERATE_JOBS in Perf Pipeline. (#6397)
    * Add GENERATE_JOBS in Perf Pipeline.
    
    Added GENERATE_JOBS in perfL2JobTemplate.
    
    Fixes: #6379
    
    Signed-off-by: Matthew Wei <mwei2@andrew.cmu.edu>
    
    * added GENERATE_JOBS logic in perfPipeline_root and perfPipeline
    
    ---------
    
    Signed-off-by: Matthew Wei <mwei2@andrew.cmu.edu>

--- buildenv/jenkins/perfPipeline_root.groovy
+++ buildenv/jenkins/perfPipeline_root.groovy
@@ -73,11 +73,17 @@ node("worker || (ci.role.test&&hw.arch.x86&&sw.os.linux)") {
 
                                 def shortName = (params.JDK_IMPL && params.JDK_IMPL == "hotspot") ? "hs" : "j9"
                                 def jobName = "Perf_openjdk${params.JDK_VERSION}_${shortName}_sanity.perf_${p}_${item.BENCHMARK}"
-                                def jobIsRunnable = JobHelper.jobIsRunnable(jobName)
-                                echo "jobName ${jobName} params: ${thisChildParams}"
-                                if (!jobIsRunnable) {
+                                if (params.GENERATE_JOBS) {
                                         echo "Generating downstream job '${jobName}' from perfL2JobTemplate …"
                                         createPerfL2Job(jobName, p, item.BENCHMARK)
+                                }
+                                else {
+                                        def jobIsRunnable = JobHelper.jobIsRunnable(jobName)
+                                        echo "jobName ${jobName} params: ${thisChildParams}"
+                                        if (!jobIsRunnable) {
+                                                echo "Generating downstream job '${jobName}' from perfL2JobTemplate …"
+                                                createPerfL2Job(jobName, p, item.BENCHMARK)
+                                        }
                                 }
                                 JOBS[jobName] = {
                                         build job: jobName, parameters: thisChildParams, propagate: true
