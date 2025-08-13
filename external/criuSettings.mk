511de4157a761249cdb6539ff979725df5e5306f
LongyuZhang <longyu.zhang@ibm.com>
2025-08-13 15:48:10 -0400

    Temporarily exclude zlinux z15 criu due to lack of machines (#6502)
    Temporarily exclude zlinux z15 criu due to lack of machines
    
    Signed-off-by: LongyuZhang <longyu.zhang@ibm.com>

--- external/criuSettings.mk
+++ external/criuSettings.mk
@@ -19,7 +19,7 @@ export CRIU_COMBO_LIST_linux_x86_64=sw.os.ubuntu.22-hw.arch.x86.broadwell sw.os.
 export CRIU_COMBO_LIST_linux_390_64_z13=sw.os.ubuntu.22-hw.arch.s390x.z13 sw.os.ubuntu.22-hw.arch.s390x.z15 sw.os.rhel.8-hw.arch.s390x.z15 sw.os.rhel.9-hw.arch.s390x.z15
 # not available: sw.os.rhel.8-hw.arch.s390x.z13
 export CRIU_COMBO_LIST_linux_390_64_z14=sw.os.ubuntu.22-hw.arch.s390x.z14 sw.os.rhel.8-hw.arch.s390x.z14 sw.os.rhel.9-hw.arch.s390x.z14 sw.os.ubuntu.22-hw.arch.s390x.z15 sw.os.rhel.8-hw.arch.s390x.z15 sw.os.rhel.9-hw.arch.s390x.z15
-export CRIU_COMBO_LIST_linux_390_64_z15=sw.os.ubuntu.22-hw.arch.s390x.z15 sw.os.rhel.8-hw.arch.s390x.z15 sw.os.rhel.9-hw.arch.s390x.z15
+# not available: export CRIU_COMBO_LIST_linux_390_64_z15=sw.os.ubuntu.22-hw.arch.s390x.z15 sw.os.rhel.8-hw.arch.s390x.z15 sw.os.rhel.9-hw.arch.s390x.z15
 
 export CRIU_COMBO_LIST_linux_aarch64=sw.os.rhel.9-hw.arch.aarch64.armv8 sw.os.rhel.8-hw.arch.aarch64.armv8 sw.os.ubuntu.22-hw.arch.aarch64.armv8
 # not available: sw.os.ubuntu.20-hw.arch.aarch64.armv8
