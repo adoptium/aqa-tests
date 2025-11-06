fc7e6ce1ca1f54afde8cf3daa2fc4f728382e5b7
Andrew Leonard <andrew.m.leonard@ibm.com>
2025-11-06 19:45:30 +0000

    Fix runtest.sh Mac minimize windows+Finder logic
    Signed-off-by: Andrew Leonard <andrew.m.leonard@ibm.com>

--- buildenv/jenkins/src/MinimizeMacWindows.java
+++ buildenv/jenkins/src/MinimizeMacWindows.java
@@ -5,64 +5,65 @@ import java.io.File;
 
 public class MinimizeMacWindows {
   public static void main(String... args) throws Exception {
+    int delay = 3000;
 
     // Minimize all windows...
     System.out.println("MinimizeMacWindows: Issuing Alt-Cmd-H to minimize all 'other' Windows");
     Robot r = new Robot();
     r.keyPress(KeyEvent.VK_META);
-    r.delay(250);
+    r.delay(delay);
     r.keyPress(KeyEvent.VK_ALT);
-    r.delay(250);
+    r.delay(delay);
     r.keyPress(KeyEvent.VK_H);
-    r.delay(250);
+    r.delay(delay);
     r.keyRelease(KeyEvent.VK_H);
-    r.delay(250);
+    r.delay(delay);
     r.keyRelease(KeyEvent.VK_ALT);
-    r.delay(250);
+    r.delay(delay);
     r.keyRelease(KeyEvent.VK_META);
-    r.delay(250);
+    r.delay(delay);
 
     System.out.println("MinimizeMacWindows: Issuing Alt-Cmd-M to minimize all 'front' Windows");
     r.keyPress(KeyEvent.VK_META);
-    r.delay(250);
+    r.delay(delay);
     r.keyPress(KeyEvent.VK_ALT);
-    r.delay(250);
+    r.delay(delay);
     r.keyPress(KeyEvent.VK_M);
-    r.delay(250);
+    r.delay(delay);
     r.keyRelease(KeyEvent.VK_M);
-    r.delay(250);
+    r.delay(delay);
     r.keyRelease(KeyEvent.VK_ALT);
-    r.delay(250);
+    r.delay(delay);
     r.keyRelease(KeyEvent.VK_META);
-    r.delay(250);
+    r.delay(delay);
 
-    // Now open and minimize all Finder windows... so that the Finder is minimized
+    // Now open and close all Finder windows... so that the Finder is closed
     System.out.println("MinimizeMacWindows: Issuing Alt-Cmd-Space open 'Finder' so as to become 'front' Window");
     r.keyPress(KeyEvent.VK_META);
-    r.delay(250);
+    r.delay(delay);
     r.keyPress(KeyEvent.VK_ALT);
-    r.delay(250);
+    r.delay(delay);
     r.keyPress(KeyEvent.VK_SPACE);
-    r.delay(250);
+    r.delay(delay);
     r.keyRelease(KeyEvent.VK_SPACE);
-    r.delay(250);
+    r.delay(delay);
     r.keyRelease(KeyEvent.VK_ALT);
-    r.delay(250);
+    r.delay(delay);
     r.keyRelease(KeyEvent.VK_META);
-    r.delay(250);
+    r.delay(delay);
 
-    System.out.println("MinimizeMacWindows: Issuing Alt-Cmd-M to minimize the 'front' 'Finder' Window");
+    System.out.println("MinimizeMacWindows: Issuing Alt-Cmd-W to close the 'front' 'Finder' Window");
     r.keyPress(KeyEvent.VK_META);
-    r.delay(250);
+    r.delay(delay);
     r.keyPress(KeyEvent.VK_ALT);
-    r.delay(250);
-    r.keyPress(KeyEvent.VK_M);
-    r.delay(250);
-    r.keyRelease(KeyEvent.VK_M);
-    r.delay(250);
+    r.delay(delay);
+    r.keyPress(KeyEvent.VK_W);
+    r.delay(delay);
+    r.keyRelease(KeyEvent.VK_W);
+    r.delay(delay);
     r.keyRelease(KeyEvent.VK_ALT);
-    r.delay(250);
+    r.delay(delay);
     r.keyRelease(KeyEvent.VK_META);
-    r.delay(250);
+    r.delay(delay);
   }
 }
