5a654e43dd10d7aa4aaaa6840bbd8dc28f332eff
Andrew Leonard <andrew.m.leonard@ibm.com>
2025-11-06 19:13:45 +0000

    Fix runtest.sh Mac minimize windows+Finder logic
    Signed-off-by: Andrew Leonard <andrew.m.leonard@ibm.com>

--- buildenv/jenkins/src/MinimizeMacWindows.java
+++ buildenv/jenkins/src/MinimizeMacWindows.java
@@ -36,7 +36,7 @@ public class MinimizeMacWindows {
     r.keyRelease(KeyEvent.VK_META);
     r.delay(250);
 
-    // Now open and close all Finder windows... so that the Finder is all closed and does not re-open on completion
+    // Now open and minimize all Finder windows... so that the Finder is minimized
     System.out.println("MinimizeMacWindows: Issuing Alt-Cmd-Space open 'Finder' so as to become 'front' Window");
     r.keyPress(KeyEvent.VK_META);
     r.delay(250);
@@ -51,14 +51,14 @@ public class MinimizeMacWindows {
     r.keyRelease(KeyEvent.VK_META);
     r.delay(250);
 
-    System.out.println("MinimizeMacWindows: Issuing Alt-Cmd-W to close 'front' 'Finder' Window");
+    System.out.println("MinimizeMacWindows: Issuing Alt-Cmd-M to minimize the 'front' 'Finder' Window");
     r.keyPress(KeyEvent.VK_META);
     r.delay(250);
     r.keyPress(KeyEvent.VK_ALT);
     r.delay(250);
-    r.keyPress(KeyEvent.VK_W);
+    r.keyPress(KeyEvent.VK_M);
     r.delay(250);
-    r.keyRelease(KeyEvent.VK_W);
+    r.keyRelease(KeyEvent.VK_M);
     r.delay(250);
     r.keyRelease(KeyEvent.VK_ALT);
     r.delay(250);
