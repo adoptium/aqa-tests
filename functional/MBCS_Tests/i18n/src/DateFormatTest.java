c49b67a793eb67e1f60eafcda0afe65985161574
Andrew Leonard <andrew.m.leonard@ibm.com>
2026-03-12 12:37:00 +0000

    Refactor ResourceBundle locale loading to be more efficient
    Signed-off-by: Andrew Leonard <andrew.m.leonard@ibm.com>

--- functional/MBCS_Tests/i18n/src/DateFormatTest.java
+++ functional/MBCS_Tests/i18n/src/DateFormatTest.java
@@ -51,7 +51,7 @@ public class DateFormatTest {
     
     private ResourceBundle tryGetBundle(String baseName, Locale locale, long version) {
         // Try loading version specific bundles first..
-        for(long i in resourceBundleFixVersions) {
+        for(long i : resourceBundleFixVersions) {
             if (i <= version) {
                 try {
                     return ResourceBundle.getBundle(baseName+"_"+i, locale);
