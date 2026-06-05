11ca4795b27f9261438e92ead7d245e5eadaeb86
Amrutha Kanhirathingal <Amrutha.Kanhirathingal@ibm.com>
2026-06-05 10:51:28 +0530

    Skip download tap.zip
    Exclude the tap.zip name from download because in test pipeline unrecognized zip is treated as SDK.
    Related: https://github.ibm.com/runtimes/automation/issues/1069
    Signed-off-by: Amrutha Kanhirathingal <Amrutha.Kanhirathingal@ibm.com>

--- get.sh
+++ get.sh
@@ -332,7 +332,7 @@ getBinaryOpenjdk()
 						if [ "$TEST_IMAGES_REQUIRED" == "true" ]; then
 							download_url+=" ${download_url_base}${n}"
 						fi
-					elif [[ $n != *"install"* && $n != *"unsigned"* ]]; then
+					elif [[ $n != *"install"* && $n != *"unsigned"* && $n != *"tap"* ]]; then
 						download_url+=" ${download_url_base}${n}"
 					fi
 				done
