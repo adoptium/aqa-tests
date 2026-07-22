0088e030ebc7db590538acf3eb091e4d8d5c5ef2
samyoon20 <samyoon20@gmail.com>
2026-07-22 10:03:36 -0400

    address PR feedback: fix shebang, quote ${TEST_HOME}, add derby_startup_test entry

--- external/tomcat/test.sh
+++ external/tomcat/test.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -20,28 +20,28 @@ TEST_TARGET="${1:-smoke}"
 
 set -e
 if [ "$TEST_TARGET" = "full" ]; then
-	git clone -q -b 1.6.x --single-branch https://github.com/apache/apr.git $TEST_HOME/tmp/apr
-	cd $TEST_HOME/tmp/apr
+	git clone -q -b 1.6.x --single-branch https://github.com/apache/apr.git "${TEST_HOME}/tmp/apr"
+	cd "${TEST_HOME}/tmp/apr"
 	./buildconf
-	./configure --prefix=$TEST_HOME/tmp/apr-build
+	./configure --prefix="${TEST_HOME}/tmp/apr-build"
 	make
 	make install
-	export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$TEST_HOME/tmp/apr-build/lib"
+	export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${TEST_HOME}/tmp/apr-build/lib"
 
-	git clone -q https://github.com/apache/tomcat-native.git $TEST_HOME/tmp/tomcat-native
-	cd $TEST_HOME/tmp/tomcat-native/native
-	sh buildconf --with-apr=$TEST_HOME/tmp/apr
-	./configure --with-apr=$TEST_HOME/tmp/apr --with-java-home=$JAVA_HOME --with-ssl=yes --prefix=$TEST_HOME/tmp/tomcat-native-build
+	git clone -q https://github.com/apache/tomcat-native.git "${TEST_HOME}/tmp/tomcat-native"
+	cd "${TEST_HOME}/tmp/tomcat-native/native"
+	sh buildconf --with-apr="${TEST_HOME}/tmp/apr"
+	./configure --with-apr="${TEST_HOME}/tmp/apr" --with-java-home=$JAVA_HOME --with-ssl=yes --prefix="${TEST_HOME}/tmp/tomcat-native-build"
 	make
 	make install
-	cd $TEST_HOME
+	cd "${TEST_HOME}"
 	yes | cp build.properties.default build.properties
 	echo >> build.properties
 	echo "test.threads=4" >> build.properties
 	echo "test.relaxTiming=true" >> build.properties
 	echo "test.excludePerformance=true" >> build.properties
 	echo "test.openssl.path=/dev/null/openssl" >> build.properties
-	echo "test.apr.loc=$TEST_HOME/tmp/tomcat-native-build/lib" >> build.properties
+	echo "test.apr.loc=${TEST_HOME}/tmp/tomcat-native-build/lib" >> build.properties
 
 	echo "Building tomcat" && \
 	ant && \
